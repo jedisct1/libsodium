e770e627dd17099480743e5157857aa2c690bcfb
Frank Denis <github@pureftpd.org>
2013-01-19 16:37:13 -0800

    libtoolize and get ready for a lot of autotools fun. Not.

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,14 @@ if [ -x "`which autoreconf 2>/dev/null`" ] ; then
   exec autoreconf -ivf
 fi
 
+if glibtoolize --version > /dev/null 2>&1; then
+  LIBTOOLIZE='glibtoolize'
+else
+  LIBTOOLIZE='libtoolize'
+fi
+
+$LIBTOOLIZE --ltdl && \
 aclocal && \
 autoheader && \
 automake --add-missing --force-missing --include-deps && \
 autoconf
-
