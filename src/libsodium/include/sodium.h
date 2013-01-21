83961aae3bdf46e7bfaaf0266ad15b7e992e553f
Frank Denis <github@pureftpd.org>
2013-01-20 16:18:36 -0800

    How about just #include <sodium.h> to include everything you need?

---
+++ src/libsodium/include/sodium.h
@@ -0,0 +1,38 @@
+
+#ifndef sodium_H
+#define sodium_H
+
+#include <sodium/crypto_auth.h>
+#include <sodium/crypto_auth_hmacsha256.h>
+#include <sodium/crypto_auth_hmacsha512256.h>
+#include <sodium/crypto_box.h>
+#include <sodium/crypto_box_curve25519xsalsa20poly1305.h>
+#include <sodium/crypto_core_hsalsa20.h>
+#include <sodium/crypto_core_salsa20.h>
+#include <sodium/crypto_core_salsa2012.h>
+#include <sodium/crypto_core_salsa208.h>
+#include <sodium/crypto_hash.h>
+#include <sodium/crypto_hash_sha256.h>
+#include <sodium/crypto_hash_sha512.h>
+#include <sodium/crypto_hashblocks_sha256.h>
+#include <sodium/crypto_hashblocks_sha512.h>
+#include <sodium/crypto_onetimeauth.h>
+#include <sodium/crypto_onetimeauth_poly1305.h>
+#include <sodium/crypto_scalarmult_curve25519.h>
+#include <sodium/crypto_secretbox.h>
+#include <sodium/crypto_secretbox_xsalsa20poly1305.h>
+#include <sodium/crypto_sign.h>
+#include <sodium/crypto_sign_ed25519.h>
+#include <sodium/crypto_sign_edwards25519sha512batch.h>
+#include <sodium/crypto_stream.h>
+#include <sodium/crypto_stream_aes128ctr.h>
+#include <sodium/crypto_stream_salsa20.h>
+#include <sodium/crypto_stream_salsa2012.h>
+#include <sodium/crypto_stream_salsa208.h>
+#include <sodium/crypto_stream_xsalsa20.h>
+#include <sodium/crypto_verify_16.h>
+#include <sodium/crypto_verify_32.h>
+#include <sodium/randombytes.h>
+#include <sodium/version.h>
+
+#endif
