c0475cc1e2ffa0a9e5840d02e4a5fab7b5bce15a
Frank Denis <github@pureftpd.org>
2013-03-11 15:15:10 -0700

    Remove donna_c64 implementation until we actually link it on 64-bit platforms

--- src/libsodium/crypto_scalarmult/curve25519/donna_c64/base.c
+++
@@ -1,8 +0,0 @@
-#include "crypto_scalarmult.h"
-
-static const unsigned char basepoint[32] = {9};
-
-int crypto_scalarmult_base(unsigned char *q,const unsigned char *n)
-{
-  return crypto_scalarmult(q, n, basepoint);
-}
