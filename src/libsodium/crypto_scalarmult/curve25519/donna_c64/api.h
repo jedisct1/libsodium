9626bbeb4447283fab40343026503f61c51e1a6a
Frank Denis <github@pureftpd.org>
2013-04-19 15:33:15 +0200

    (re)import curve25519_donna_c64

---
+++ src/libsodium/crypto_scalarmult/curve25519/donna_c64/api.h
@@ -0,0 +1,12 @@
+#ifndef crypto_scalarmult_H
+#define crypto_scalarmult_H
+
+#include "crypto_scalarmult_curve25519.h"
+
+#define crypto_scalarmult_curve25519_implementation_name \
+    crypto_scalarmult_curve25519_donna_c64_implementation_name
+
+#define crypto_scalarmult crypto_scalarmult_curve25519_donna_c64
+#define crypto_scalarmult_base crypto_scalarmult_curve25519_donna_c64_base
+
+#endif
