c46e08585b931f20557dfb3186784ab804bbf03a
Frank Denis <github@pureftpd.org>
2013-04-19 18:15:06 +0200

    Wrap crypto_secretbox

---
+++ src/libsodium/crypto_secretbox/crypto_secretbox.c
@@ -0,0 +1,18 @@
+
+#include "crypto_secretbox.h"
+
+int
+crypto_secretbox(unsigned char *c, const unsigned char *m,
+                 unsigned long long mlen, const unsigned char *n,
+                 const unsigned char *k)
+{
+    return crypto_secretbox_xsalsa20poly1305(c, m, mlen, n, k);
+}
+
+int
+crypto_secretbox_open(unsigned char *m, const unsigned char *c,
+                      unsigned long long clen, const unsigned char *n,
+                      const unsigned char *k)
+{
+    return crypto_secretbox_xsalsa20poly1305_open(m, c, clen, n, k);
+}
--- src/libsodium/include/sodium/crypto_secretbox.h
+++ src/libsodium/include/sodium/crypto_secretbox.h
@@ -3,12 +3,18 @@
 
 #include "crypto_secretbox_xsalsa20poly1305.h"
 
-#define crypto_secretbox crypto_secretbox_xsalsa20poly1305
-#define crypto_secretbox_open crypto_secretbox_xsalsa20poly1305_open
 #define crypto_secretbox_KEYBYTES crypto_secretbox_xsalsa20poly1305_KEYBYTES
 #define crypto_secretbox_NONCEBYTES crypto_secretbox_xsalsa20poly1305_NONCEBYTES
 #define crypto_secretbox_ZEROBYTES crypto_secretbox_xsalsa20poly1305_ZEROBYTES
 #define crypto_secretbox_BOXZEROBYTES crypto_secretbox_xsalsa20poly1305_BOXZEROBYTES
 #define crypto_secretbox_PRIMITIVE "xsalsa20poly1305"
+
+int crypto_secretbox(unsigned char *c, const unsigned char *m,
+                     unsigned long long mlen, const unsigned char *n,
+                     const unsigned char *k);
+
+int crypto_secretbox_open(unsigned char *m, const unsigned char *c,
+                          unsigned long long clen, const unsigned char *n,
+                          const unsigned char *k);
 
 #endif
