8a5165bf068c4af94562c311069f796af036c8d5
Frank Denis <github@pureftpd.org>
2013-04-19 17:30:17 +0200

    Start replacing macros with real functions. First candidate: crypto_auth

--- src/libsodium/include/sodium/crypto_auth.h
+++ src/libsodium/include/sodium/crypto_auth.h
@@ -3,10 +3,20 @@
 
 #include "crypto_auth_hmacsha512256.h"
 
-#define crypto_auth crypto_auth_hmacsha512256
-#define crypto_auth_verify crypto_auth_hmacsha512256_verify
 #define crypto_auth_BYTES crypto_auth_hmacsha512256_BYTES
 #define crypto_auth_KEYBYTES crypto_auth_hmacsha512256_KEYBYTES
 #define crypto_auth_PRIMITIVE "hmacsha512256"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int crypto_auth(unsigned char *out, const unsigned char *in,
+                unsigned long long inlen, const unsigned char *k);
+
+int crypto_auth_verify(const unsigned char *h, const unsigned char *in,
+                       unsigned long long inlen,const unsigned char *k);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
