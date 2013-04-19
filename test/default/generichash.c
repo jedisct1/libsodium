d43309490a4b2b074b74b96e4e418d626e1815fd
Frank Denis <github@pureftpd.org>
2013-04-20 00:28:19 +0200

    Add preliminary tests for crypto_generichash (blake2)

---
+++ test/default/generichash.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "crypto_uint8.h"
+
+#define TEST_NAME "generichash"
+#include "cmptest.h"
+
+int main(void)
+{
+#define MAXLEN 64
+    crypto_uint8 in[MAXLEN], out[crypto_generichash_BYTES_MAX], k[crypto_generichash_KEYBYTES_MAX];
+    int h,i,j;
+
+    for(h = 0; h < crypto_generichash_KEYBYTES_MAX; ++h) k[h] = h;
+
+    for(i = 0; i < MAXLEN; ++i) {
+        in[i]=i;
+        crypto_generichash(out, in, k, 1 + i % crypto_generichash_BYTES_MAX,
+                           i, 1 + i % crypto_generichash_KEYBYTES_MAX);
+        for (j = 0;j < 1 + i % crypto_generichash_BYTES_MAX;++j) {
+            printf("%02x",(unsigned int) out[j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
