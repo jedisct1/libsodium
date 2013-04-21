4a9a9ffeb879b5f135f88c4daf938316af391716
Frank Denis <github@pureftpd.org>
2013-04-21 16:34:50 +0200

    Wrap crypto_scalarmult macros.

--- src/libsodium/crypto_scalarmult/crypto_scalarmult.c
+++ src/libsodium/crypto_scalarmult/crypto_scalarmult.c
@@ -1,6 +1,24 @@
 
 #include "crypto_scalarmult.h"
 
+size_t
+crypto_scalarmult_bytes(void)
+{
+    return crypto_scalarmult_BYTES;
+}
+    
+size_t
+crypto_scalarmult_scalarbytes(void)
+{
+    return crypto_scalarmult_SCALARBYTES;
+}
+
+const char *
+crypto_scalarmult_primitive(void)
+{
+    return crypto_scalarmult_PRIMITIVE;
+}
+
 int
 crypto_scalarmult_base(unsigned char *q, const unsigned char *n)
 {
--- src/libsodium/include/sodium/crypto_scalarmult.h
+++ src/libsodium/include/sodium/crypto_scalarmult.h
@@ -1,15 +1,22 @@
 #ifndef crypto_scalarmult_H
 #define crypto_scalarmult_H
 
-#include "crypto_scalarmult_curve25519.h"
+#include <stdlib.h>
 
-#define crypto_scalarmult_BYTES crypto_scalarmult_curve25519_BYTES
-#define crypto_scalarmult_SCALARBYTES crypto_scalarmult_curve25519_SCALARBYTES
-#define crypto_scalarmult_PRIMITIVE "curve25519"
+#include "crypto_scalarmult_curve25519.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define crypto_scalarmult_BYTES crypto_scalarmult_curve25519_BYTES
+size_t  crypto_scalarmult_bytes(void);
+    
+#define crypto_scalarmult_SCALARBYTES crypto_scalarmult_curve25519_SCALARBYTES
+size_t  crypto_scalarmult_scalarbytes(void);
+
+#define crypto_scalarmult_PRIMITIVE "curve25519"
+const char *crypto_scalarmult_primitive(void);
 
 int crypto_scalarmult_base(unsigned char *q, const unsigned char *n);
 
