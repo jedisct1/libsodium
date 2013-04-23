0b4affc43615a4ea581bcd0b478e6942dcbdd693
Frank Denis <github@pureftpd.org>
2013-04-23 14:28:51 -0700

    Add a big fat warning to crypto_stream*

--- src/libsodium/include/sodium/crypto_stream_aes128ctr.h
+++ src/libsodium/include/sodium/crypto_stream_aes128ctr.h
@@ -1,6 +1,14 @@
 #ifndef crypto_stream_aes128ctr_H
 #define crypto_stream_aes128ctr_H
 
+/*
+ *  WARNING: This is just a stream cipher. It is NOT authenticated encryption.
+ *  While it provides some protection against eavesdropping, it does NOT
+ *  provide any security against active attacks.
+ *  Unless you know what you're doing, what you are looking for is probably
+ *  the crypto_box functions.
+ */
+
 #include "export.h"
 
 #define crypto_stream_aes128ctr_KEYBYTES 16
@@ -10,7 +18,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 SODIUM_EXPORT
 int crypto_stream_aes128ctr(unsigned char *,unsigned long long,const unsigned char *,const unsigned char *);
--- src/libsodium/include/sodium/crypto_stream_salsa208.h
+++ src/libsodium/include/sodium/crypto_stream_salsa208.h
@@ -1,6 +1,14 @@
 #ifndef crypto_stream_salsa208_H
 #define crypto_stream_salsa208_H
 
+/*
+ *  WARNING: This is just a stream cipher. It is NOT authenticated encryption.
+ *  While it provides some protection against eavesdropping, it does NOT
+ *  provide any security against active attacks.
+ *  Unless you know what you're doing, what you are looking for is probably
+ *  the crypto_box functions.
+ */
+
 #include "export.h"
 
 #define crypto_stream_salsa208_KEYBYTES 32
