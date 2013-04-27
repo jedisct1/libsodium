1b87626e9f7de6e17dce75aa5fd51ba5ca7b8e33
Frank Denis <github@pureftpd.org>
2013-04-26 17:05:23 -0700

    Move randombytes_stir() before returning sodium_init().

--- src/libsodium/sodium/core.c
+++ src/libsodium/sodium/core.c
@@ -12,10 +12,10 @@ sodium_init(void)
     if (initialized != 0) {
         return 1;
     }
-    randombytes_stir();
     if (crypto_onetimeauth_pick_best_implementation() == NULL) {
         return -1;
     }
+    randombytes_stir();
     initialized = 1;
 
     return 0;
