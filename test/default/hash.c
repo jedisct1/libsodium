47de75e935d774e6ae08882ec5c4311d9c0a72fa
Frank Denis <github@pureftpd.org>
2013-04-26 21:52:21 -0700

    Keep ming Visual C++ happy

--- test/default/hash.c
+++ test/default/hash.c
@@ -3,13 +3,13 @@
 #define TEST_NAME "hash"
 #include "cmptest.h"
 
-unsigned char x[8] = "testing\n";
+unsigned char x[] = "testing\n";
 unsigned char h[crypto_hash_BYTES];
 
 int main(void)
 {
   int i;
-  crypto_hash(h,x,sizeof x);
+  crypto_hash(h,x,sizeof x - 1U);
   for (i = 0;i < crypto_hash_BYTES;++i) printf("%02x",(unsigned int) h[i]);
   printf("\n");
   return 0;
--- test/default/hash3.c
+++ test/default/hash3.c
@@ -3,13 +3,13 @@
 #define TEST_NAME "hash3"
 #include "cmptest.h"
 
-unsigned char x[8] = "testing\n";
+unsigned char x[] = "testing\n";
 unsigned char h[crypto_hash_sha512_BYTES];
 
 int main(void)
 {
   int i;
-  crypto_hash_sha512(h,x,sizeof x);
+  crypto_hash_sha512(h,x,sizeof x - 1U);
   for (i = 0;i < crypto_hash_sha512_BYTES;++i) printf("%02x",(unsigned int) h[i]);
   printf("\n");
   return 0;
