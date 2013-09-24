ad49d05a8bd19b43f9758d82793c46e2a233e687
Frank Denis <github@pureftpd.org>
2013-09-24 14:06:26 -0700

    Match randombytes() prototype with declaration.

--- src/libsodium/include/sodium/randombytes.h
+++ src/libsodium/include/sodium/randombytes.h
@@ -26,7 +26,7 @@ SODIUM_EXPORT
 int         randombytes_set_implementation(randombytes_implementation *impl);
 
 SODIUM_EXPORT
-void        randombytes(unsigned char *buf, unsigned long long size);
+void        randombytes(unsigned char * const buf, const unsigned long long buf_len);
 
 SODIUM_EXPORT
 const char *randombytes_implementation_name(void);
