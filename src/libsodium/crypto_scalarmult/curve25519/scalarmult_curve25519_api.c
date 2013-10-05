8effa1ea884bffcf445414303ccfe8be30e57cf5
Frank Denis <github@pureftpd.org>
2013-10-05 14:06:09 -0700

    Export scalarmult_curve25519 constants

--- src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519_api.c
+++ src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519_api.c
@@ -1,1 +1,13 @@
 #include "crypto_scalarmult_curve25519.h"
+
+size_t
+crypto_scalarmult_curve25519_bytes(void)
+{
+    return crypto_scalarmult_curve25519_BYTES;
+}
+
+size_t
+crypto_scalarmult_curve25519_scalarbytes(void)
+{
+    return crypto_scalarmult_curve25519_SCALARBYTES;
+}
