e9c46aef5f5ea207c41675aeb5e92df284b0312a
Frank Denis <github@pureftpd.org>
2013-11-05 21:46:19 -0800

    Add support for salt & personalization to Blake2. This API might change. I'm not a big fan of what we have now.

--- src/libsodium/crypto_generichash/blake2/ref/blake2b-ref.c
+++ src/libsodium/crypto_generichash/blake2/ref/blake2b-ref.c
@@ -184,6 +184,34 @@ int blake2b_init( blake2b_state *S, const uint8_t outlen )
   return blake2b_init_param( S, P );
 }
 
+int blake2b_init_salt_personal( blake2b_state *S, const uint8_t outlen,
+                                const void *salt, const void *personal)
+{
+  blake2b_param P[1];
+
+  if ( ( !outlen ) || ( outlen > BLAKE2B_OUTBYTES ) ) return -1;
+
+  P->digest_length = outlen;
+  P->key_length    = 0;
+  P->fanout        = 1;
+  P->depth         = 1;
+  store32( &P->leaf_length, 0 );
+  store64( &P->node_offset, 0 );
+  P->node_depth    = 0;
+  P->inner_length  = 0;
+  memset( P->reserved, 0, sizeof( P->reserved ) );
+  if (salt != NULL) {
+    blake2b_param_set_salt( P, salt );
+  } else {
+    memset( P->salt, 0, sizeof( P->salt ) );
+  }
+  if (personal != NULL) {
+    blake2b_param_set_personal( P, personal );
+  } else {
+    memset( P->personal, 0, sizeof( P->personal ) );
+  }
+  return blake2b_init_param( S, P );
+}
 
 int blake2b_init_key( blake2b_state *S, const uint8_t outlen, const void *key, const uint8_t keylen )
 {
@@ -204,6 +232,47 @@ int blake2b_init_key( blake2b_state *S, const uint8_t outlen, const void *key, c
   memset( P->reserved, 0, sizeof( P->reserved ) );
   memset( P->salt,     0, sizeof( P->salt ) );
   memset( P->personal, 0, sizeof( P->personal ) );
+
+  if( blake2b_init_param( S, P ) < 0 ) return -1;
+
+  {
+    uint8_t block[BLAKE2B_BLOCKBYTES];
+    memset( block, 0, BLAKE2B_BLOCKBYTES );
+    memcpy( block, key, keylen );
+    blake2b_update( S, block, BLAKE2B_BLOCKBYTES );
+    secure_zero_memory( block, BLAKE2B_BLOCKBYTES ); /* Burn the key from stack */
+  }
+  return 0;
+}
+
+int blake2b_init_key_salt_personal( blake2b_state *S, const uint8_t outlen, const void *key, const uint8_t keylen,
+                                    const void *salt, const void *personal )
+{
+  blake2b_param P[1];
+
+  if ( ( !outlen ) || ( outlen > BLAKE2B_OUTBYTES ) ) return -1;
+
+  if ( !key || !keylen || keylen > BLAKE2B_KEYBYTES ) return -1;
+
+  P->digest_length = outlen;
+  P->key_length    = keylen;
+  P->fanout        = 1;
+  P->depth         = 1;
+  store32( &P->leaf_length, 0 );
+  store64( &P->node_offset, 0 );
+  P->node_depth    = 0;
+  P->inner_length  = 0;
+  memset( P->reserved, 0, sizeof( P->reserved ) );
+  if (salt != NULL) {
+    blake2b_param_set_salt( P, salt );
+  } else {
+    memset( P->salt, 0, sizeof( P->salt ) );
+  }
+  if (personal != NULL) {
+    blake2b_param_set_personal( P, personal );
+  } else {
+    memset( P->personal, 0, sizeof( P->personal ) );
+  }
 
   if( blake2b_init_param( S, P ) < 0 ) return -1;
 
--- src/libsodium/crypto_generichash/blake2/ref/generichash_blake2b.c
+++ src/libsodium/crypto_generichash/blake2/ref/generichash_blake2b.c
@@ -44,6 +44,32 @@ crypto_generichash_blake2b_init(crypto_generichash_blake2b_state *state,
 }
 
 int
+crypto_generichash_blake2b_init2(crypto_generichash_blake2b_state *state,
+                                 const unsigned char *key,
+                                 const size_t keylen, const size_t outlen,
+                                 const unsigned char *salt,
+                                 const unsigned char *personal)
+{
+    if (outlen <= 0U || outlen > BLAKE2B_OUTBYTES ||
+        keylen > BLAKE2B_KEYBYTES) {
+        return -1;
+    }
+    assert(outlen <= UINT8_MAX);
+    assert(keylen <= UINT8_MAX);
+    if (key == NULL || keylen <= 0U) {
+        if (blake2b_init_salt_personal(state, (uint8_t) outlen,
+                                       salt, personal) != 0) {
+            return -1;
+        }
+    } else if (blake2b_init_key_salt_personal(state,
+                                              (uint8_t) outlen, key, keylen,
+                                              salt, personal) != 0) {
+        return -1;
+    }
+    return 0;
+}
+
+int
 crypto_generichash_blake2b_update(crypto_generichash_blake2b_state *state,
                                   const unsigned char *in,
                                   unsigned long long inlen)
