2994aebafb4e9434979863e4f8ac845365e07272
Frank Denis <github@pureftpd.org>
2013-12-11 20:27:42 -0800

    Remove unused macro

--- src/libsodium/crypto_stream/salsa20/ref/api.h
+++ src/libsodium/crypto_stream/salsa20/ref/api.h
@@ -1,8 +1,5 @@
 
 #include "crypto_stream_salsa20.h"
 
-#define crypto_stream_salsa20_implementation_name \
-    crypto_stream_salsa20_ref_implementation_name
-
 #define crypto_stream crypto_stream_salsa20
 #define crypto_stream_xor crypto_stream_salsa20_xor
