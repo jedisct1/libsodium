eab6ff79d4edbeef44a53a58ff8d56773e599e9e
Frank Denis <github@pureftpd.org>
2013-12-30 17:54:16 +0100

    Add a build script for msys-win64, rename msys.sh to msys-win32.sh

---
+++ dist-build/msys-win64.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+export CFLAGS="-O3 -fomit-frame-pointer -m64"
+export PREFIX="$(pwd)/libsodium-win64"
+
+./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
+            --host=x86_64-w64-mingw32 && \
+make && \
+make check && \
+make install
