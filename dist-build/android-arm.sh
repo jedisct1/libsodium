2c48c844d4cff527736c346bc50d122bf0bce193
Sergey Shatunov <prototypegamez@gmail.com>
2014-02-24 17:16:35 +0800

    Split for armv6 and armv7 archs in buildscripts

--- dist-build/android-arm.sh
+++ dist-build/android-arm.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-export CFLAGS="-Os -mthumb"
-export LDFLAGS="-mthumb"
+export CFLAGS="-Os -mthumb -marm -march=armv6"
 TARGET_ARCH=arm HOST_COMPILER=arm-linux-androideabi "$(dirname "$0")/android-build.sh"
--- dist-build/android-mips.sh
+++ dist-build/android-mips.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export CFLAGS="-Os"
 TARGET_ARCH=mips HOST_COMPILER=mipsel-linux-android "$(dirname "$0")/android-build.sh"
