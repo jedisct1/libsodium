5e9c894d137e1d13ba77e1d2fffaac20e3d4b035
Frank Denis <github@pureftpd.org>
2014-04-14 21:32:01 -0700

    Add tests for auth_hmac_sha512

---
+++ test/default/auth6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define TEST_NAME "auth6"
+#include "cmptest.h"
+
+/* "Test Case 2" from RFC 4231 */
+unsigned char key[32] = "Jefe";
+unsigned char c[] = "what do ya want for nothing?";
+
+unsigned char a[64];
+
+int main(void)
+{
+  int i;
+  crypto_auth_hmacsha512(a,c,sizeof c - 1U,key);
+  for (i = 0;i < 64;++i) {
+    printf(",0x%02x",(unsigned int) a[i]);
+    if (i % 8 == 7) printf("\n");
+  }
+  return 0;
+}
