c6f1a2670a7c9192193ddaf65b6edf267d3093a4
Frank Denis <github@pureftpd.org>
2014-04-15 00:39:14 -0700

    Add sodium_hex2bin()

--- test/default/sodium_utils.c
+++ test/default/sodium_utils.c
@@ -9,6 +9,9 @@ int main(void)
   unsigned char buf1[1000];
   unsigned char buf2[1000];
   char          buf3[33];
+  unsigned char buf4[4];
+  const char   *hex;
+  size_t        bin_len;
 
   randombytes(buf1, sizeof buf1);
   memcpy(buf2, buf1, sizeof buf2);
@@ -23,5 +26,10 @@ int main(void)
   printf("%s\n", sodium_bin2hex(buf3, 33U,
                                 (const unsigned char *)
                                 "0123456789ABCDEF", 16U));
+  hex = "Cafe : 6942";
+  sodium_hex2bin(buf4, sizeof buf4, hex, strlen(hex), ": ", &bin_len, NULL);
+  printf("%zu:%02x%02x%02x%02x\n", bin_len,
+         buf4[0], buf4[1], buf4[2], buf4[3]);
+
   return 0;
 }
