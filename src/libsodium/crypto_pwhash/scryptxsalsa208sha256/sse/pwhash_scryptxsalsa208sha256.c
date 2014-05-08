3785fc5cd505a040e411326cf939eb236faffada
Jan Varho <jan@varho.org>
2014-05-08 12:48:56 +0300

    Check r and p against zero before dividing

--- src/libsodium/crypto_pwhash/scryptxsalsa208sha256/sse/pwhash_scryptxsalsa208sha256.c
+++ src/libsodium/crypto_pwhash/scryptxsalsa208sha256/sse/pwhash_scryptxsalsa208sha256.c
@@ -334,6 +334,10 @@ escrypt_kdf_sse(escrypt_local_t * local,
 		errno = EINVAL;
 		return -1;
 	}
+	if (r == 0 || p == 0) {
+		errno = EINVAL;
+		return -1;
+	}
 	if ((r > SIZE_MAX / 128 / p) ||
 #if SIZE_MAX / 256 <= UINT32_MAX
 	    (r > SIZE_MAX / 256) ||
