9383d59e2181b93be6801303a17a74c5361c2aa0
Frank Denis <github@pureftpd.org>
2014-05-08 21:32:36 -0700

    It's totally fine for sodium_library_version_minor() to be 0.

--- test/default/sodium_version.c
+++ test/default/sodium_version.c
@@ -7,7 +7,7 @@ int main(void)
 {
   printf ("%d\n", sodium_version_string() != NULL);
   printf ("%d\n", sodium_library_version_major() > 0);
-  printf ("%d\n", sodium_library_version_minor() > 0);
+  printf ("%d\n", sodium_library_version_minor() >= 0);
 
   return 0;
 }
