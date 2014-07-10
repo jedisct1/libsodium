e6e0b7d49821d5982909f8d98b8f59442168afea
Frank Denis <github@pureftpd.org>
2014-07-09 21:14:27 -0700

    Use --enable-minimal with the Javascript build

--- dist-build/emscripten.sh
+++ dist-build/emscripten.sh
@@ -6,7 +6,7 @@ export OPTFLAGS="--llvm-lto 3 -Oz"
 export CFLAGS="$OPTFLAGS --pre-js=test/default/pre.js "
 export LDFLAGS="-s EXPORTED_FUNCTIONS=${EXPORTED_FUNCTIONS}"
 
-emconfigure ./configure --disable-shared --prefix="$PREFIX" && \
+emconfigure ./configure --enable-minimal --disable-shared --prefix="$PREFIX" && \
 emmake make clean &&
 emmake make install &&
 emcc --closure 1 $OPTFLAGS $LDFLAGS \
