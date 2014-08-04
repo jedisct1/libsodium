c5a9d46386f917aa0ff1bfb711450f9af1d79a17
Frank Denis <github@pureftpd.org>
2014-08-03 21:15:04 -0700

    Double check for crypto_auth_verify()

--- src/libsodium/crypto_auth/hmacsha256/cp/verify_hmacsha256.c
+++ src/libsodium/crypto_auth/hmacsha256/cp/verify_hmacsha256.c
@@ -1,9 +1,11 @@
 #include "api.h"
 #include "crypto_verify_32.h"
+#include "utils.h"
 
 int crypto_auth_verify(const unsigned char *h,const unsigned char *in,unsigned long long inlen,const unsigned char *k)
 {
   unsigned char correct[32];
   crypto_auth(correct,in,inlen,k);
-  return crypto_verify_32(h,correct);
+  return crypto_verify_32(h,correct) | (-(h - correct == 0)) |
+         sodium_memcmp(correct,h,32);
 }
--- src/libsodium/crypto_auth/hmacsha512/cp/verify_hmacsha512.c
+++ src/libsodium/crypto_auth/hmacsha512/cp/verify_hmacsha512.c
@@ -1,10 +1,12 @@
 #include "api.h"
 #include "crypto_verify_64.h"
+#include "utils.h"
 
 int crypto_auth_verify(const unsigned char *h, const unsigned char *in,
                        unsigned long long inlen, const unsigned char *k)
 {
   unsigned char correct[64];
   crypto_auth(correct,in,inlen,k);
-  return crypto_verify_64(h,correct);
+  return crypto_verify_64(h,correct) | (-(h - correct == 0)) |
+         sodium_memcmp(correct,h,64);
 }
