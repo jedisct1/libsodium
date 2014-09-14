0ec5a25c8857e98f05a3de846ccf15e42d674442
Frank Denis <github@pureftpd.org>
2014-09-14 12:43:53 -0700

    More tests for randombytes

--- test/default/randombytes.c
+++ test/default/randombytes.c
@@ -1,12 +1,15 @@
-#include "randombytes.h"
+
+#define TEST_NAME "randombytes"
+#include "cmptest.h"
 
 unsigned char x[65536];
 unsigned long long freq[256];
 
-static int nacl_tests(void)
+static int compat_tests(void)
 {
-    unsigned long long i;
+    size_t i;
 
+    memset(x, 0, sizeof x);
     randombytes(x, sizeof x);
     for (i = 0; i < 256; ++i) {
         freq[i] = 0;
@@ -16,14 +19,66 @@ static int nacl_tests(void)
     }
     for (i = 0; i < 256; ++i) {
         if (!freq[i]) {
-            return 111;
+            printf("nacl_tests failed\n");
         }
     }
+    return 0;
+}
+
+static int randombytes_tests(void)
+{
+    unsigned int i;
+
+    assert(strcmp(randombytes_implementation_name(), "sysrandom") == 0);
+    for (i = 0; i < 256; ++i) {
+        freq[i] = 0;
+    }
+    for (i = 0; i < 65536; ++i) {
+        ++freq[randombytes_uniform(256)];
+    }
+    for (i = 0; i < 256; ++i) {
+        if (!freq[i]) {
+            printf("randombytes_uniform() test failed\n");
+        }
+    }
+    randombytes_close();
+    randombytes_set_implementation(&randombytes_salsa20_implementation);
+    assert(strcmp(randombytes_implementation_name(), "salsa20") == 0);
+    randombytes_stir();
+    for (i = 0; i < 256; ++i) {
+        freq[i] = 0;
+    }
+    for (i = 0; i < 65536; ++i) {
+        ++freq[randombytes_uniform(256)];
+    }
+    for (i = 0; i < 256; ++i) {
+        if (!freq[i]) {
+            printf("randombytes_uniform() test failed\n");
+        }
+    }
+    memset(x, 0, sizeof x);
+    randombytes_buf(x, sizeof x);
+    for (i = 0; i < 256; ++i) {
+        freq[i] = 0;
+    }
+    for (i = 0; i < sizeof x; ++i) {
+        ++freq[255 & (int)x[i]];
+    }
+    for (i = 0; i < 256; ++i) {
+        if (!freq[i]) {
+            printf("randombytes_buf() test failed\n");
+        }
+    }
+    randombytes_close();
+
+    return 0;
 }
 
 int main(void)
 {
-    nacl_tests();
+    compat_tests();
+    randombytes_tests();
+    printf("OK\n");
 
     return 0;
 }
