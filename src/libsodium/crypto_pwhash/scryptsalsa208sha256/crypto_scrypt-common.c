f71c1c0e178aca9c310d602afeccf149e75127d5
Frank Denis <github@pureftpd.org>
2014-09-23 21:19:49 -0700

    lcov exclusion

--- src/libsodium/crypto_pwhash/scryptsalsa208sha256/crypto_scrypt-common.c
+++ src/libsodium/crypto_pwhash/scryptsalsa208sha256/crypto_scrypt-common.c
@@ -36,7 +36,7 @@ encode64_uint32(uint8_t * dst, size_t dstlen, uint32_t src, uint32_t srcbits)
 
     for (bit = 0; bit < srcbits; bit += 6) {
         if (dstlen < 1) {
-            return NULL;
+            return NULL; /* LCOV_EXCL_LINE */
         }
         *dst++ = itoa64[src & 0x3f];
         dstlen--;
