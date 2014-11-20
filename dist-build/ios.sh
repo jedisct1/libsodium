2ee447ff1412878dd69d0692e256b8ddd4269939
Frank Denis <github@pureftpd.org>
2014-11-20 14:01:35 -0800

    iOS: add -miphoneos-version-min

--- dist-build/ios.sh
+++ dist-build/ios.sh
@@ -3,9 +3,10 @@
 export XCODEDIR=$(xcode-select -p)
 export BASEDIR="${XCODEDIR}/Platforms/iPhoneOS.platform/Developer"
 export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
-export SDK="${BASEDIR}/SDKs/iPhoneOS8.0.sdk"
-export CFLAGS="-Oz -mthumb -arch armv7 -isysroot ${SDK}"
-export LDFLAGS="-mthumb -arch armv7 -isysroot ${SDK}"
+export SDK="${BASEDIR}/SDKs/iPhoneOS.sdk"
+export IPHONEOS_VERSION_MIN="5.1.1"
+export CFLAGS="-Oz -mthumb -arch armv7 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
+export LDFLAGS="-mthumb -arch armv7 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
 export PREFIX="$(pwd)/libsodium-ios"
 
 ./configure --host=arm-apple-darwin10 \
