34c639c6805d57d85a885710ab747d54dcbc44e1
Syrian Watermelon <batikhsouri@gmail.com>
2014-12-05 08:37:46 +0100

    Removing auto-conversion to hex based on a regex. Allowed types are Uint8Array and strings

--- dist-build/js-build/macros/input_buf.js
+++ dist-build/js-build/macros/input_buf.js
@@ -4,10 +4,7 @@ var {var_name}_size = {var_size};
 if ({var_name} instanceof Uint8Array){
 	if ({var_name}.length != {var_size}) throw new TypeError('invalid {var_name} size');
 	{var_name}_byte = injectBytes({var_name});
-} else if (is_hex({var_name})){
-	if ({var_name}.length / 2 != {var_size}) throw new TypeError('invalid {var_name} size');
-	{var_name}_byte = injectBytes(from_hex({var_name}));
-} else {
+} else if (typeof {var_name} == 'string'){
 	if (encode_utf8({var_name}).length != {var_size}) throw new TypeError('invalid {var_name} size');
 	{var_name}_byte = injectBytes(encode_utf8({var_name}));
-}
+} else throw new TypeError('Illegal input type for {var_name}');
--- dist-build/js-build/macros/input_unsized_buf.js
+++ dist-build/js-build/macros/input_unsized_buf.js
@@ -2,9 +2,6 @@ var {var_name}_byte;
 var {var_name}_size = {var_name}.length;
 if ({var_name} instanceof Uint8Array){
 	{var_name}_byte = injectBytes({var_name});
-} else if (is_hex({var_name})){
-	{var_name}_byte = injectBytes(from_hex({var_name}));
-	{var_name}_size /= 2;
-} else {
+} else if (typeof {var_name} == 'string'){
 	{var_name}_byte = injectBytes(encode_utf8({var_name}));
-}
+} else throw new TypeError('Illegal input type for {var_name}');
