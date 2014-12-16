9af34c57660d7126a705337ff076bb10a0f7ef48
Frank Denis <github@pureftpd.org>
2014-12-16 19:33:58 +0100

    Compile for armv7s

--- dist-build/ios.sh
+++ dist-build/ios.sh
@@ -4,7 +4,7 @@
 #  Configure for base system so simulator is covered
 #  
 #  Step 2.
-#  Make for iOS arm7 and arm64
+#  Make for iOS armv7, armv7s and arm64
 #
 #  Step 3.
 #  Merge libs into final version for xcode import
@@ -44,8 +44,8 @@ export BASEDIR="${XCODEDIR}/Platforms/iPhoneOS.platform/Developer"
 export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
 export SDK="${BASEDIR}/SDKs/iPhoneOS.sdk"
 export IPHONEOS_VERSION_MIN="5.1.1"
-export CFLAGS="-Oz -mthumb -arch armv7 -arch arm64 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
-export LDFLAGS="-mthumb -arch armv7 -arch arm64 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
+export CFLAGS="-Oz -mthumb -arch armv7 -arch armv7s -arch arm64 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
+export LDFLAGS="-mthumb -arch armv7 -arch armv7s -arch arm64 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
 
 ./configure --host=arm-apple-darwin10 \
             --disable-shared \
