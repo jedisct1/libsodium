33269d0f2eb8469e88affaa7e3fe1ced863012bf
Syrian Watermelon <batikhsouri@gmail.com>
2014-12-17 10:11:50 +0100

    Possibility not to inject new line feeds in base64 encoded strings

--- dist-build/js-build/wrap-template.js
+++ dist-build/js-build/wrap-template.js
@@ -113,11 +113,11 @@ var libsodium = (function () {
 			65;
 	}
 
-	function base64EncArr (aBytes) {
+	function base64EncArr (aBytes, noNewLine) {
 		var nMod3 = 2, sB64Enc = "";
 		for (var nLen = aBytes.length, nUint24 = 0, nIdx = 0; nIdx < nLen; nIdx++) {
 			nMod3 = nIdx % 3;
-			if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0) { sB64Enc += "\r\n"; }
+			if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0 && !noNewLine) { sB64Enc += "\r\n"; }
 			nUint24 |= aBytes[nIdx] << (16 >>> nMod3 & 24);
 			if (nMod3 === 2 || aBytes.length - nIdx === 1) {
 			sB64Enc += String.fromCharCode(uint6ToB64(nUint24 >>> 18 & 63), uint6ToB64(nUint24 >>> 12 & 63), uint6ToB64(nUint24 >>> 6 & 63), uint6ToB64(nUint24 & 63));
