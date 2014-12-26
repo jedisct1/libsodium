c7492a4baef7599f397fce576e9d74d59ee455c2
Frank Denis <github@pureftpd.org>
2014-12-26 23:01:38 +0100

    Make IPHONEOS_VERSION_MIN overridable

--- dist-build/ios.sh
+++ dist-build/ios.sh
@@ -43,7 +43,7 @@ export XCODEDIR=$(xcode-select -p)
 export BASEDIR="${XCODEDIR}/Platforms/iPhoneOS.platform/Developer"
 export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
 export SDK="${BASEDIR}/SDKs/iPhoneOS.sdk"
-export IPHONEOS_VERSION_MIN="5.1.1"
+export IPHONEOS_VERSION_MIN=${IPHONEOS_VERSION_MIN-"5.1.1"}
 export CFLAGS="-Oz -mthumb -arch armv7 -arch armv7s -arch arm64 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
 export LDFLAGS="-mthumb -arch armv7 -arch armv7s -arch arm64 -isysroot ${SDK} -miphoneos-version-min=${IPHONEOS_VERSION_MIN}"
 
