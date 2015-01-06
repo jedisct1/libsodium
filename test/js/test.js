4c039277622ed4c94e6d2da2267b989735a45999
Syrian Watermelon <batikhsouri@gmail.com>
2015-01-06 01:07:54 +0100

    Added testing (+vector) for crypto_secretbox_easy

--- test/js/test.js
+++ test/js/test.js
@@ -117,5 +117,29 @@ var libsodium_test = (function(){
 
 	};
 
+	t.secretbox_easy = function(){
+		var vectors = [
+			{
+				key: '1b27556473e985d462cd51197a9a46c76009549eac6474f206c4ee0844f68389',
+				nonce: '69696ee955b62b73cd62bda875fc73d68219e0036b7a0b37',
+				m: 'be075fc53c81f2d5cf141316ebeb0c7b5228c52a4c62cbd44b66849b64244ffce5ecbaaf33bd751a1ac728d45e6c61296cdc3c01233561f41db66cce314adb310e3be8250c46f06dceea3a7fa1348057e2f6556ad6b1318a024a838f21af1fde048977eb48f59ffd4924ca1c60902e52f0a089bc76897040e082f937763848645e0705',
+				c: 'f3ffc7703f9400e52a7dfb4b3d3305d98e993b9f48681273c29650ba32fc76ce48332ea7164d96a4476fb8c531a1186ac0dfc17c98dce87b4da7f011ec48c97271d2c20f9b928fe2270d6fb863d51738b48eeee314a7cc8ab932164548e526ae90224368517acfeabd6bb3732bc0e9da99832b61ca01b6de56244a9e88d5f9b37973f622a43d14a6599b1f654cb45a74e355a5'
+			}
+		];
+
+		for (var i = 0; i < vectors.length; i++){
+			console.log(JSON.stringify(vectors[i]));
+			secretbox_test(vectors[i]);
+		}
+
+		function secretbox_test(v){
+			var cipher = libsodium.crypto_secretbox_easy(from_hex(v.m), from_hex(v.nonce), from_hex(v.key));
+			if (to_hex(cipher) != v.c) throw new Error('Unexpected ciphertext');
+			var plaintext = libsodium.crypto_secretbox_open_easy(cipher, from_hex(v.nonce), from_hex(v.key));
+			console.log(to_hex(plaintext));
+			if (!(plaintext && to_hex(plaintext) == v.m)) throw new Error('Unexpected plaintext');
+		}
+	}
+
 	return t;
 })();
