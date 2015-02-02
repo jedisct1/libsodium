8ad5d61c18faad9a7e938e9cf459e49037a2205e
Cyberunner23 <narutoxela@gmail.com>
2015-02-02 10:44:09 -0500

    Fixed that fail... Tests run fine now.

--- src/libsodium/crypto_stream/xsalsa20/stream_xsalsa20_api.c
+++ src/libsodium/crypto_stream/xsalsa20/stream_xsalsa20_api.c
@@ -16,5 +16,5 @@ int crypto_stream_xsalsa20_xor(unsigned char *c, const unsigned char *m,
                                unsigned long long mlen, const unsigned char *n,
                                const unsigned char *k)
 {
-    return crypto_stream_salsa20_xor_ic(c, m, mlen, n, 0U, k);
+    return crypto_stream_xsalsa20_xor_ic(c, m, mlen, n, 0U, k);
 }
