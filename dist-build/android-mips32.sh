53a5914a4c06ecd0ce6303318343081bed285bca
Frank Denis <github@pureftpd.org>
2015-02-08 10:42:27 +0100

    android-mips -> android-mips32

--- dist-build/android-mips32.sh
+++ dist-build/android-mips32.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export TARGET_ARCH=mipsel
+export TARGET_ARCH=mips32
 export CFLAGS="-Os -march=${TARGET_ARCH}"
 ARCH=mips HOST_COMPILER=mipsel-linux-android "$(dirname "$0")/android-build.sh"
