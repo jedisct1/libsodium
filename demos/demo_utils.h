b92cc46432f41801114ad3576defd8f92944d067
Frank Denis <github@pureftpd.org>
2015-05-27 15:40:37 +0200

    Crank up the max input size; use stddef.h instead of stdlib.h for size_t

--- demos/demo_utils.h
+++ demos/demo_utils.h
@@ -4,9 +4,9 @@
 #ifndef DEMO_UTILS_H
 #define DEMO_UTILS_H
 
-#include <stdlib.h>
+#include <stddef.h>
 
-#define MAX_INPUT_SIZE 128 /* size of all input buffers in the demo */
+#define MAX_INPUT_SIZE 4096 /* max size of all input buffers in the demo */
 
 void print_hex(const void *bin, const size_t bin_len);
 size_t prompt_input(char *prompt, char *input, const size_t max_input_len);
