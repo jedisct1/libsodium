ec7b8e56058fe65b4b62402aa9314935b8ca8810
Frank Denis <github@pureftpd.org>
2015-10-14 14:43:22 +0200

    Version bump [but still not released]

--- builds/msvc/version.h
+++ builds/msvc/version.h
@@ -4,10 +4,10 @@
 
 #include "export.h"
 
-#define SODIUM_VERSION_STRING "1.0.3"
+#define SODIUM_VERSION_STRING "1.0.4"
 
 #define SODIUM_LIBRARY_VERSION_MAJOR 7
-#define SODIUM_LIBRARY_VERSION_MINOR 5
+#define SODIUM_LIBRARY_VERSION_MINOR 6
 
 #ifdef __cplusplus
 extern "C" {
