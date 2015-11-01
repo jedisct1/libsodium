1d4a2aabac82edc29ddf2df7f23914e0b15c826a
Frank Denis <github@pureftpd.org>
2015-11-01 12:51:04 +0100

    Reindent

--- src/libsodium/crypto_onetimeauth/poly1305/onetimeauth_poly1305.h
+++ src/libsodium/crypto_onetimeauth/poly1305/onetimeauth_poly1305.h
@@ -3,21 +3,21 @@
 #define onetimeauth_poly1305_H
 
 typedef struct crypto_onetimeauth_poly1305_implementation {
-    int         (*onetimeauth)(unsigned char *out,
-                               const unsigned char *in,
-                               unsigned long long inlen,
-                               const unsigned char *k);
-    int         (*onetimeauth_verify)(const unsigned char *h,
-                                      const unsigned char *in,
-                                      unsigned long long inlen,
-                                      const unsigned char *k);
-    int         (*onetimeauth_init)(crypto_onetimeauth_poly1305_state *state,
-                                    const unsigned char *key);
-    int         (*onetimeauth_update)(crypto_onetimeauth_poly1305_state *state,
-                                      const unsigned char *in,
-                                      unsigned long long inlen);
-    int         (*onetimeauth_final)(crypto_onetimeauth_poly1305_state *state,
-                                     unsigned char *out);
+    int (*onetimeauth)(unsigned char *out,
+                       const unsigned char *in,
+                       unsigned long long inlen,
+                       const unsigned char *k);
+    int (*onetimeauth_verify)(const unsigned char *h,
+                              const unsigned char *in,
+                              unsigned long long inlen,
+                              const unsigned char *k);
+    int (*onetimeauth_init)(crypto_onetimeauth_poly1305_state *state,
+                            const unsigned char *key);
+    int (*onetimeauth_update)(crypto_onetimeauth_poly1305_state *state,
+                              const unsigned char *in,
+                              unsigned long long inlen);
+    int (*onetimeauth_final)(crypto_onetimeauth_poly1305_state *state,
+                             unsigned char *out);
 } crypto_onetimeauth_poly1305_implementation;
 
 #endif
