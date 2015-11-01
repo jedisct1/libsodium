7f238f4c8bf4fbe6a7b87b2a5296f4892e7d139c
Frank Denis <github@pureftpd.org>
2015-11-01 02:09:22 +0100

    Assume that optimized Blake2b versions can use at least SSSE3

--- src/libsodium/crypto_generichash/blake2/sse/blake2b-round.h
+++ src/libsodium/crypto_generichash/blake2/sse/blake2b-round.h
@@ -10,7 +10,7 @@
    You should have received a copy of the CC0 Public Domain Dedication along with
    this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 */
-#pragma once
+
 #ifndef __BLAKE2B_ROUND_H__
 #define __BLAKE2B_ROUND_H__
 
@@ -20,21 +20,14 @@
 #define TOF(reg) _mm_castsi128_ps((reg))
 #define TOI(reg) _mm_castps_si128((reg))
 
-#define LIKELY(x) __builtin_expect((x),1)
-
 
 /* Microarchitecture-specific macros */
-#ifdef HAVE_SSSE3
 #define _mm_roti_epi64(x, c) \
     (-(c) == 32) ? _mm_shuffle_epi32((x), _MM_SHUFFLE(2,3,0,1))  \
     : (-(c) == 24) ? _mm_shuffle_epi8((x), r24) \
     : (-(c) == 16) ? _mm_shuffle_epi8((x), r16) \
     : (-(c) == 63) ? _mm_xor_si128(_mm_srli_epi64((x), -(c)), _mm_add_epi64((x), (x)))  \
     : _mm_xor_si128(_mm_srli_epi64((x), -(c)), _mm_slli_epi64((x), 64-(-(c))))
-#else
-/* ... */
-#endif
-
 
 
 #define G1(row1l,row2l,row3l,row4l,row1h,row2h,row3h,row4h,b0,b1) \
@@ -75,7 +68,6 @@
   row2l = _mm_roti_epi64(row2l, -63); \
   row2h = _mm_roti_epi64(row2h, -63); \
  
-#if defined(HAVE_SSSE3)
 #define DIAGONALIZE(row1l,row2l,row3l,row4l,row1h,row2h,row3h,row4h) \
   t0 = _mm_alignr_epi8(row2h, row2l, 8); \
   t1 = _mm_alignr_epi8(row2l, row2h, 8); \
@@ -105,33 +97,8 @@
   t1 = _mm_alignr_epi8(row4h, row4l, 8); \
   row4l = t1; \
   row4h = t0;
-#else
 
-#define DIAGONALIZE(row1l,row2l,row3l,row4l,row1h,row2h,row3h,row4h) \
-  t0 = row4l;\
-  t1 = row2l;\
-  row4l = row3l;\
-  row3l = row3h;\
-  row3h = row4l;\
-  row4l = _mm_unpackhi_epi64(row4h, _mm_unpacklo_epi64(t0, t0)); \
-  row4h = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(row4h, row4h)); \
-  row2l = _mm_unpackhi_epi64(row2l, _mm_unpacklo_epi64(row2h, row2h)); \
-  row2h = _mm_unpackhi_epi64(row2h, _mm_unpacklo_epi64(t1, t1))
-
-#define UNDIAGONALIZE(row1l,row2l,row3l,row4l,row1h,row2h,row3h,row4h) \
-  t0 = row3l;\
-  row3l = row3h;\
-  row3h = t0;\
-  t0 = row2l;\
-  t1 = row4l;\
-  row2l = _mm_unpackhi_epi64(row2h, _mm_unpacklo_epi64(row2l, row2l)); \
-  row2h = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(row2h, row2h)); \
-  row4l = _mm_unpackhi_epi64(row4l, _mm_unpacklo_epi64(row4h, row4h)); \
-  row4h = _mm_unpackhi_epi64(row4h, _mm_unpacklo_epi64(t1, t1))
-
-#endif
-
-#if defined(HAVE_SSE41)
+#if defined(BLAKE2_USE_SSE41)
 #include "blake2b-load-sse41.h"
 #else
 #include "blake2b-load-sse2.h"
@@ -150,4 +117,3 @@
   UNDIAGONALIZE(row1l,row2l,row3l,row4l,row1h,row2h,row3h,row4h);
 
 #endif
-
