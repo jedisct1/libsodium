e7bb1e200e5a9ec3397f561e4c5cdf8682604283
Frank Denis <github@pureftpd.org>
2015-11-01 01:27:10 +0100

    Do not export crypto_onetimeauth_poly1305_set_implementation() This is a relique from very early versions of libsodium that should eventually just go away.

--- src/libsodium/include/sodium/crypto_onetimeauth_poly1305.h
+++ src/libsodium/include/sodium/crypto_onetimeauth_poly1305.h
@@ -21,25 +21,6 @@ typedef struct crypto_onetimeauth_poly1305_state {
     unsigned char      opaque[136];
 } crypto_onetimeauth_poly1305_state;
 
-typedef struct crypto_onetimeauth_poly1305_implementation {
-    const char *(*implementation_name)(void);
-    int         (*onetimeauth)(unsigned char *out,
-                               const unsigned char *in,
-                               unsigned long long inlen,
-                               const unsigned char *k);
-    int         (*onetimeauth_verify)(const unsigned char *h,
-                                      const unsigned char *in,
-                                      unsigned long long inlen,
-                                      const unsigned char *k);
-    int         (*onetimeauth_init)(crypto_onetimeauth_poly1305_state *state,
-                                    const unsigned char *key);
-    int         (*onetimeauth_update)(crypto_onetimeauth_poly1305_state *state,
-                                      const unsigned char *in,
-                                      unsigned long long inlen);
-    int         (*onetimeauth_final)(crypto_onetimeauth_poly1305_state *state,
-                                     unsigned char *out);
-} crypto_onetimeauth_poly1305_implementation;
-
 #define crypto_onetimeauth_poly1305_BYTES 16U
 SODIUM_EXPORT
 size_t crypto_onetimeauth_poly1305_bytes(void);
@@ -47,15 +28,6 @@ size_t crypto_onetimeauth_poly1305_bytes(void);
 #define crypto_onetimeauth_poly1305_KEYBYTES 32U
 SODIUM_EXPORT
 size_t crypto_onetimeauth_poly1305_keybytes(void);
-
-SODIUM_EXPORT
-const char *crypto_onetimeauth_poly1305_implementation_name(void);
-
-SODIUM_EXPORT
-int crypto_onetimeauth_poly1305_set_implementation(crypto_onetimeauth_poly1305_implementation *impl);
-
-crypto_onetimeauth_poly1305_implementation *
-crypto_onetimeauth_pick_best_implementation(void);
 
 SODIUM_EXPORT
 int crypto_onetimeauth_poly1305(unsigned char *out,
@@ -81,6 +53,33 @@ int crypto_onetimeauth_poly1305_update(crypto_onetimeauth_poly1305_state *state,
 SODIUM_EXPORT
 int crypto_onetimeauth_poly1305_final(crypto_onetimeauth_poly1305_state *state,
                                       unsigned char *out);
+
+const char *crypto_onetimeauth_poly1305_implementation_name(void);
+
+/* ------------------------------------------------------------------------- */
+
+typedef struct crypto_onetimeauth_poly1305_implementation {
+    const char *(*implementation_name)(void);
+    int         (*onetimeauth)(unsigned char *out,
+                               const unsigned char *in,
+                               unsigned long long inlen,
+                               const unsigned char *k);
+    int         (*onetimeauth_verify)(const unsigned char *h,
+                                      const unsigned char *in,
+                                      unsigned long long inlen,
+                                      const unsigned char *k);
+    int         (*onetimeauth_init)(crypto_onetimeauth_poly1305_state *state,
+                                    const unsigned char *key);
+    int         (*onetimeauth_update)(crypto_onetimeauth_poly1305_state *state,
+                                      const unsigned char *in,
+                                      unsigned long long inlen);
+    int         (*onetimeauth_final)(crypto_onetimeauth_poly1305_state *state,
+                                     unsigned char *out);
+} crypto_onetimeauth_poly1305_implementation;
+
+int crypto_onetimeauth_poly1305_set_implementation(crypto_onetimeauth_poly1305_implementation *impl);
+
+crypto_onetimeauth_poly1305_implementation *crypto_onetimeauth_pick_best_implementation(void);
 
 #ifdef __cplusplus
 }
