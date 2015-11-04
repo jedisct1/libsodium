91fc9e298a2a6016b059b08b2a33fa422f1f5d61
Frank Denis <github@pureftpd.org>
2015-11-04 19:19:20 +0100

    C++ compat

--- src/libsodium/crypto_scalarmult/curve25519/sandy2x/fe51.h
+++ src/libsodium/crypto_scalarmult/curve25519/sandy2x/fe51.h
@@ -8,6 +8,10 @@
 #ifndef fe51_H
 #define fe51_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "crypto_uint64.h"
 #include "fe51_namespace.h"
 
@@ -21,5 +25,9 @@ extern void fe51_pack(unsigned char *, const fe51 *);
 extern void fe51_mul(fe51 *, const fe51 *, const fe51 *);
 extern void fe51_nsquare(fe51 *, const fe51 *, int);
 extern void fe51_invert(fe51 *, const fe51 *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
