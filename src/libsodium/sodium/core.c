18910cf43ad85b91be96387d7980c65b269e17fb
Frank Denis <github@pureftpd.org>
2015-11-04 16:56:47 +0100

    Let sodium_init() pick the fastest curve25519 implementation

--- src/libsodium/sodium/core.c
+++ src/libsodium/sodium/core.c
@@ -2,6 +2,7 @@
 #include "core.h"
 #include "crypto_generichash.h"
 #include "crypto_onetimeauth.h"
+#include "crypto_scalarmult.h"
 #include "randombytes.h"
 #include "runtime.h"
 #include "utils.h"
@@ -19,6 +20,7 @@ sodium_init(void)
     _sodium_alloc_init();
     _crypto_generichash_blake2b_pick_best_implementation();
     _crypto_onetimeauth_poly1305_pick_best_implementation();
+    _crypto_scalarmult_curve25519_pick_best_implementation();
     initialized = 1;
 
     return 0;
