cc29da17c39b0cd881de4fe5d154841057f75f91
Frank Denis <github@pureftpd.org>
2015-11-11 12:41:41 +0100

    Use minimal builds on msys2

--- dist-build/msys2-win64.sh
+++ dist-build/msys2-win64.sh
@@ -3,7 +3,8 @@
 export CFLAGS="-O3 -fomit-frame-pointer -m64 -mtune=nocona"
 export PREFIX="$(pwd)/libsodium-win64"
 
-./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
+./configure --enable-minimal \
+            --prefix="$PREFIX" --exec-prefix="$PREFIX" \
             --host=x86_64-w64-mingw32 && \
 make && \
 make check && \
