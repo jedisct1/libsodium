dd238f518d32a42433200d5b50aeed280d5a49fa
Frank Denis <github@pureftpd.org>
2015-11-14 15:33:15 +0100

    Bump major

--- msvc-scripts/process.bat
+++ msvc-scripts/process.bat
@@ -1,4 +1,4 @@
 cscript msvc-scripts/rep.vbs //Nologo s/@VERSION@/1.0.6/ < src\libsodium\include\sodium\version.h.in > tmp
-cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_VERSION_MAJOR@/8/ < tmp > tmp2
+cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_VERSION_MAJOR@/9/ < tmp > tmp2
 cscript msvc-scripts/rep.vbs //Nologo s/@SODIUM_LIBRARY_VERSION_MINOR@/0/ < tmp2 > src\libsodium\include\sodium\version.h
 del tmp tmp2
