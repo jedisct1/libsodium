f40a432bf5f6178b2a0fbb07b38ad2ae06a49c58
Frank Denis <github@pureftpd.org>
2015-11-19 17:05:28 +0100

    More informative messages about missing package

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,33 @@
 #! /bin/sh
 
-if [ -x "`which autoreconf 2>/dev/null`" ] ; then
-  exec autoreconf -ivf
-fi
-
 if glibtoolize --version > /dev/null 2>&1; then
   LIBTOOLIZE='glibtoolize'
 else
   LIBTOOLIZE='libtoolize'
+fi
+
+if [ ! -x "`which $LIBTOOLIZE 2>/dev/null`" ] ; then
+  echo "libtool is required, but wasn't found on this system"
+  exit 1
+fi
+
+if [ ! -x "`which autoconf 2>/dev/null`" ] ; then
+  echo "autoconf is required, but wasn't found on this system"
+  exit 1
+fi
+
+if [ ! -x "`which automake 2>/dev/null`" ] ; then
+  echo "automake is required, but wasn't found on this system"
+  exit 1
+fi
+
+if [ ! -x "`which pkg-config 2>/dev/null`" ] ; then
+  echo "pkg-config is required, but wasn't found on this system"
+  exit 1
+fi
+
+if [ -x "`which autoreconf 2>/dev/null`" ] ; then
+  exec autoreconf -ivf
 fi
 
 $LIBTOOLIZE && \
