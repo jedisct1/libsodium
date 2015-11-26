c160dbc9cd1f6273eed964368cc76760133620c2
Frank Denis <github@pureftpd.org>
2015-11-26 22:34:21 +0100

    Use uint128_t consistently

--- src/libsodium/crypto_generichash/blake2/ref/blake2b-ref.c
+++ src/libsodium/crypto_generichash/blake2/ref/blake2b-ref.c
@@ -72,7 +72,7 @@ static inline int blake2b_clear_lastblock( blake2b_state *S )
 static inline int blake2b_increment_counter( blake2b_state *S, const uint64_t inc )
 {
 #ifdef HAVE_TI_MODE
-  uint128_t t = ( ( __uint128_t )S->t[1] << 64 ) | S->t[0];
+  uint128_t t = ( ( uint128_t )S->t[1] << 64 ) | S->t[0];
   t += inc;
   S->t[0] = ( uint64_t )( t >>  0 );
   S->t[1] = ( uint64_t )( t >> 64 );
