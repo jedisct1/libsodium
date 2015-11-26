a5b4926a19fb69c0dd64df858b3587ba0ac0c5e4
Frank Denis <github@pureftpd.org>
2015-11-26 17:06:18 +0100

    Keep it simple to avoid issues with the different heaps in Emscripten

--- src/libsodium/crypto_verify/16/ref/verify_16.c
+++ src/libsodium/crypto_verify/16/ref/verify_16.c
@@ -7,16 +7,6 @@
 int
 crypto_verify_16(const unsigned char *x, const unsigned char *y)
 {
-#ifdef CPU_UNALIGNED_ACCESS
-    uint_fast64_t d = 0U;
-    int           i;
-
-    for (i = 0; i < 16; i += 4) {
-        d |= *((const uint32_t *) (const void *) &x[i]) ^
-             *((const uint32_t *) (const void *) &y[i]);
-    }
-    return (1 & ((d - 1) >> 32)) - 1;
-#else
     uint_fast16_t d = 0U;
     int           i;
 
@@ -24,5 +14,4 @@ crypto_verify_16(const unsigned char *x, const unsigned char *y)
         d |= x[i] ^ y[i];
     }
     return (1 & ((d - 1) >> 8)) - 1;
-#endif
 }
