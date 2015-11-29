bc531c6af51d055f818f82a323cc65d76dd5b15c
Frank Denis <github@pureftpd.org>
2015-11-30 00:08:00 +0100

    On OSX, compile with -flto for better performance

--- dist-build/osx.sh
+++ dist-build/osx.sh
@@ -6,8 +6,8 @@ export OSX_CPU_ARCH=${OSX_CPU_ARCH-"core2"}
 
 mkdir -p $PREFIX || exit 1
 
-export CFLAGS="-arch x86_64 -mmacosx-version-min=${OSX_VERSION_MIN} -march=${OSX_CPU_ARCH} -O2 -g"
-export LDFLAGS="-arch x86_64 -mmacosx-version-min=${OSX_VERSION_MIN} -march=${OSX_CPU_ARCH}"
+export CFLAGS="-arch x86_64 -mmacosx-version-min=${OSX_VERSION_MIN} -march=${OSX_CPU_ARCH} -O2 -g -flto"
+export LDFLAGS="-arch x86_64 -mmacosx-version-min=${OSX_VERSION_MIN} -march=${OSX_CPU_ARCH} -flto"
 
 make distclean > /dev/null
 
