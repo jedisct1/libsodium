7bee56450b68ad133d5a59b72eb8d26d0a656c17
Frank Denis <github@pureftpd.org>
2015-12-09 22:29:48 +0100

    Revert "Use minimal builds on msys2"
    This reverts commit cc29da17c39b0cd881de4fe5d154841057f75f91.

--- dist-build/msys2-win64.sh
+++ dist-build/msys2-win64.sh
@@ -3,8 +3,7 @@
 export CFLAGS="-O3 -fomit-frame-pointer -m64 -mtune=nocona"
 export PREFIX="$(pwd)/libsodium-win64"
 
-./configure --enable-minimal \
-            --prefix="$PREFIX" --exec-prefix="$PREFIX" \
+./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
             --host=x86_64-w64-mingw32 && \
 make && \
 make check && \
