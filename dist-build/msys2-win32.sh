e7d9129729cba1d7c2e728dae59d032d0d0a0e71
Frank Denis <github@pureftpd.org>
2015-12-10 10:26:15 +0100

    Run make distclean, not just make clean in the msys2 build scripts for consistency with other build scripts

--- dist-build/msys2-win32.sh
+++ dist-build/msys2-win32.sh
@@ -3,9 +3,10 @@
 export CFLAGS="-O3 -fomit-frame-pointer -m32 -march=pentium2 -mtune=nocona"
 export PREFIX="$(pwd)/libsodium-win32"
 
+make distclean > /dev/null
+
 ./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
             --host=i686-w64-mingw32 && \
-make clean && \
 make && \
 make check && \
 make install
--- dist-build/msys2-win64.sh
+++ dist-build/msys2-win64.sh
@@ -3,9 +3,10 @@
 export CFLAGS="-O3 -fomit-frame-pointer -m64 -mtune=nocona"
 export PREFIX="$(pwd)/libsodium-win64"
 
+make distclean > /dev/null
+
 ./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
             --host=x86_64-w64-mingw32 && \
-make clean && \
 make && \
 make check && \
 make install
