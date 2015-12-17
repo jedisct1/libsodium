7fb442eb1eb522ce221533f4329580b99b4d1ce6
Frank Denis <github@pureftpd.org>
2015-12-17 17:26:36 +0100

    Finish replacing shifts on integers with multiplications No binary changes

--- src/libsodium/crypto_core/curve25519/ref10/curve25519_ref10.c
+++ src/libsodium/crypto_core/curve25519/ref10/curve25519_ref10.c
@@ -213,15 +213,15 @@ Ignores top bit of h.
 void fe_frombytes(fe h,const unsigned char *s)
 {
   crypto_int64 h0 = load_4(s);
-  crypto_int64 h1 = load_3(s + 4) << 6;
-  crypto_int64 h2 = load_3(s + 7) << 5;
-  crypto_int64 h3 = load_3(s + 10) << 3;
-  crypto_int64 h4 = load_3(s + 13) << 2;
+  crypto_int64 h1 = load_3(s + 4) * (1L << 6);
+  crypto_int64 h2 = load_3(s + 7) * (1L << 5);
+  crypto_int64 h3 = load_3(s + 10) * (1L << 3);
+  crypto_int64 h4 = load_3(s + 13) * (1L << 2);
   crypto_int64 h5 = load_4(s + 16);
-  crypto_int64 h6 = load_3(s + 20) << 7;
-  crypto_int64 h7 = load_3(s + 23) << 5;
-  crypto_int64 h8 = load_3(s + 26) << 4;
-  crypto_int64 h9 = (load_3(s + 29) & 8388607) << 2;
+  crypto_int64 h6 = load_3(s + 20) * (1L << 7);
+  crypto_int64 h7 = load_3(s + 23) * (1L << 5);
+  crypto_int64 h8 = load_3(s + 26) * (1L << 4);
+  crypto_int64 h9 = (load_3(s + 29) & 8388607) * (1L << 2);
   crypto_int64 carry0;
   crypto_int64 carry1;
   crypto_int64 carry2;
@@ -233,17 +233,17 @@ void fe_frombytes(fe h,const unsigned char *s)
   crypto_int64 carry8;
   crypto_int64 carry9;
 
-  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 << 25;
-  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 << 25;
-  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 << 25;
-  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 << 25;
-  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 << 25;
+  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 * (1L << 25);
+  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 * (1L << 25);
+  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 * (1L << 25);
+  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 * (1L << 25);
+  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 * (1L << 25);
 
-  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 << 26;
-  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 << 26;
-  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 << 26;
-  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 << 26;
-  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 << 26;
+  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 * (1L << 26);
+  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 * (1L << 26);
+  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 * (1L << 26);
+  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 * (1L << 26);
+  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 * (1L << 26);
 
   h[0] = (crypto_int32) h0;
   h[1] = (crypto_int32) h1;
@@ -322,16 +322,16 @@ void fe_tobytes(unsigned char *s,const fe h)
   h0 += 19 * q;
   /* Goal: Output h-2^255 q, which is between 0 and 2^255-20. */
 
-  carry0 = h0 >> 26; h1 += carry0; h0 -= carry0 << 26;
-  carry1 = h1 >> 25; h2 += carry1; h1 -= carry1 << 25;
-  carry2 = h2 >> 26; h3 += carry2; h2 -= carry2 << 26;
-  carry3 = h3 >> 25; h4 += carry3; h3 -= carry3 << 25;
-  carry4 = h4 >> 26; h5 += carry4; h4 -= carry4 << 26;
-  carry5 = h5 >> 25; h6 += carry5; h5 -= carry5 << 25;
-  carry6 = h6 >> 26; h7 += carry6; h6 -= carry6 << 26;
-  carry7 = h7 >> 25; h8 += carry7; h7 -= carry7 << 25;
-  carry8 = h8 >> 26; h9 += carry8; h8 -= carry8 << 26;
-  carry9 = h9 >> 25;               h9 -= carry9 << 25;
+  carry0 = h0 >> 26; h1 += carry0; h0 -= carry0 * (1 << 26);
+  carry1 = h1 >> 25; h2 += carry1; h1 -= carry1 * (1 << 25);
+  carry2 = h2 >> 26; h3 += carry2; h2 -= carry2 * (1 << 26);
+  carry3 = h3 >> 25; h4 += carry3; h3 -= carry3 * (1 << 25);
+  carry4 = h4 >> 26; h5 += carry4; h4 -= carry4 * (1 << 26);
+  carry5 = h5 >> 25; h6 += carry5; h5 -= carry5 * (1 << 25);
+  carry6 = h6 >> 26; h7 += carry6; h6 -= carry6 * (1 << 26);
+  carry7 = h7 >> 25; h8 += carry7; h7 -= carry7 * (1 << 25);
+  carry8 = h8 >> 26; h9 += carry8; h8 -= carry8 * (1 << 26);
+  carry9 = h9 >> 25;               h9 -= carry9 * (1 << 25);
                   /* h10 = carry9 */
 
   /*
@@ -344,32 +344,32 @@ void fe_tobytes(unsigned char *s,const fe h)
   s[0] = h0 >> 0;
   s[1] = h0 >> 8;
   s[2] = h0 >> 16;
-  s[3] = (h0 >> 24) | (h1 << 2);
+  s[3] = (h0 >> 24) | (h1 * (1 << 2));
   s[4] = h1 >> 6;
   s[5] = h1 >> 14;
-  s[6] = (h1 >> 22) | (h2 << 3);
+  s[6] = (h1 >> 22) | (h2 * (1 << 3));
   s[7] = h2 >> 5;
   s[8] = h2 >> 13;
-  s[9] = (h2 >> 21) | (h3 << 5);
+  s[9] = (h2 >> 21) | (h3 * (1 << 5));
   s[10] = h3 >> 3;
   s[11] = h3 >> 11;
-  s[12] = (h3 >> 19) | (h4 << 6);
+  s[12] = (h3 >> 19) | (h4 * (1 << 6));
   s[13] = h4 >> 2;
   s[14] = h4 >> 10;
   s[15] = h4 >> 18;
   s[16] = h5 >> 0;
   s[17] = h5 >> 8;
   s[18] = h5 >> 16;
-  s[19] = (h5 >> 24) | (h6 << 1);
+  s[19] = (h5 >> 24) | (h6 * (1 << 1));
   s[20] = h6 >> 7;
   s[21] = h6 >> 15;
-  s[22] = (h6 >> 23) | (h7 << 3);
+  s[22] = (h6 >> 23) | (h7 * (1 << 3));
   s[23] = h7 >> 5;
   s[24] = h7 >> 13;
-  s[25] = (h7 >> 21) | (h8 << 4);
+  s[25] = (h7 >> 21) | (h8 * (1 << 4));
   s[26] = h8 >> 4;
   s[27] = h8 >> 12;
-  s[28] = (h8 >> 20) | (h9 << 6);
+  s[28] = (h8 >> 20) | (h9 * (1 << 6));
   s[29] = h9 >> 2;
   s[30] = h9 >> 10;
   s[31] = h9 >> 18;
@@ -603,46 +603,46 @@ void fe_mul(fe h,const fe f,const fe g)
     i.e. |h1| <= 1.7*2^59; narrower ranges for h3, h5, h7, h9
   */
 
-  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 << 26;
-  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 << 26;
+  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 * (1L << 26);
+  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 * (1L << 26);
   /* |h0| <= 2^25 */
   /* |h4| <= 2^25 */
   /* |h1| <= 1.71*2^59 */
   /* |h5| <= 1.71*2^59 */
 
-  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 << 25;
-  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 << 25;
+  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 * (1L << 25);
+  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 * (1L << 25);
   /* |h1| <= 2^24; from now on fits into int32 */
   /* |h5| <= 2^24; from now on fits into int32 */
   /* |h2| <= 1.41*2^60 */
   /* |h6| <= 1.41*2^60 */
 
-  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 << 26;
-  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 << 26;
+  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 * (1L << 26);
+  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 * (1L << 26);
   /* |h2| <= 2^25; from now on fits into int32 unchanged */
   /* |h6| <= 2^25; from now on fits into int32 unchanged */
   /* |h3| <= 1.71*2^59 */
   /* |h7| <= 1.71*2^59 */
 
-  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 << 25;
-  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 << 25;
+  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 * (1L << 25);
+  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 * (1L << 25);
   /* |h3| <= 2^24; from now on fits into int32 unchanged */
   /* |h7| <= 2^24; from now on fits into int32 unchanged */
   /* |h4| <= 1.72*2^34 */
   /* |h8| <= 1.41*2^60 */
 
-  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 << 26;
-  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 << 26;
+  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 * (1L << 26);
+  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 * (1L << 26);
   /* |h4| <= 2^25; from now on fits into int32 unchanged */
   /* |h8| <= 2^25; from now on fits into int32 unchanged */
   /* |h5| <= 1.01*2^24 */
   /* |h9| <= 1.71*2^59 */
 
-  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 << 25;
+  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 * (1L << 25);
   /* |h9| <= 2^24; from now on fits into int32 unchanged */
   /* |h0| <= 1.1*2^39 */
 
-  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 << 26;
+  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 * (1L << 26);
   /* |h0| <= 2^25; from now on fits into int32 unchanged */
   /* |h1| <= 1.01*2^24 */
 
@@ -818,24 +818,24 @@ void fe_sq(fe h,const fe f)
   crypto_int64 carry8;
   crypto_int64 carry9;
 
-  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 << 26;
-  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 << 26;
+  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 * (1L << 26);
+  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 * (1L << 26);
 
-  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 << 25;
-  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 << 25;
+  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 * (1L << 25);
+  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 * (1L << 25);
 
-  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 << 26;
-  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 << 26;
+  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 * (1L << 26);
+  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 * (1L << 26);
 
-  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 << 25;
-  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 << 25;
+  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 * (1L << 25);
+  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 * (1L << 25);
 
-  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 << 26;
-  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 << 26;
+  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 * (1L << 26);
+  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 * (1L << 26);
 
-  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 << 25;
+  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 * (1L << 25);
 
-  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 << 26;
+  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 * (1L << 26);
 
   h[0] = (crypto_int32) h0;
   h[1] = (crypto_int32) h1;
@@ -976,24 +976,24 @@ void fe_sq2(fe h,const fe f)
   h8 += h8;
   h9 += h9;
 
-  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 << 26;
-  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 << 26;
+  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 * (1L << 26);
+  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 * (1L << 26);
 
-  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 << 25;
-  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 << 25;
+  carry1 = (h1 + (crypto_int64) (1L << 24)) >> 25; h2 += carry1; h1 -= carry1 * (1L << 25);
+  carry5 = (h5 + (crypto_int64) (1L << 24)) >> 25; h6 += carry5; h5 -= carry5 * (1L << 25);
 
-  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 << 26;
-  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 << 26;
+  carry2 = (h2 + (crypto_int64) (1L << 25)) >> 26; h3 += carry2; h2 -= carry2 * (1L << 26);
+  carry6 = (h6 + (crypto_int64) (1L << 25)) >> 26; h7 += carry6; h6 -= carry6 * (1L << 26);
 
-  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 << 25;
-  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 << 25;
+  carry3 = (h3 + (crypto_int64) (1L << 24)) >> 25; h4 += carry3; h3 -= carry3 * (1L << 25);
+  carry7 = (h7 + (crypto_int64) (1L << 24)) >> 25; h8 += carry7; h7 -= carry7 * (1L << 25);
 
-  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 << 26;
-  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 << 26;
+  carry4 = (h4 + (crypto_int64) (1L << 25)) >> 26; h5 += carry4; h4 -= carry4 * (1L << 26);
+  carry8 = (h8 + (crypto_int64) (1L << 25)) >> 26; h9 += carry8; h8 -= carry8 * (1L << 26);
 
-  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 << 25;
+  carry9 = (h9 + (crypto_int64) (1L << 24)) >> 25; h0 += carry9 * 19; h9 -= carry9 * (1L << 25);
 
-  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 << 26;
+  carry0 = (h0 + (crypto_int64) (1L << 25)) >> 26; h1 += carry0; h0 -= carry0 * (1L << 26);
 
   h[0] = (crypto_int32) h0;
   h[1] = (crypto_int32) h1;
@@ -1226,9 +1226,9 @@ static void slide(signed char *r,const unsigned char *a)
     if (r[i]) {
       for (b = 1;b <= 6 && i + b < 256;++b) {
         if (r[i + b]) {
-          if (r[i] + (r[i + b] << b) <= 15) {
+          if (r[i] + (r[i + b] * (1 << b)) <= 15) {
             r[i] += r[i + b] << b; r[i + b] = 0;
-          } else if (r[i] - (r[i + b] << b) >= -15) {
+          } else if (r[i] - (r[i + b] * (1 << b)) >= -15) {
             r[i] -= r[i + b] << b;
             for (k = i + b;k < 256;++k) {
               if (!r[k]) {
@@ -1992,33 +1992,33 @@ void sc_muladd(unsigned char *s,const unsigned char *a,const unsigned char *b,co
 
   s[0] = s0 >> 0;
   s[1] = s0 >> 8;
-  s[2] = (s0 >> 16) | (s1 << 5);
+  s[2] = (s0 >> 16) | (s1 * (1L << 5));
   s[3] = s1 >> 3;
   s[4] = s1 >> 11;
-  s[5] = (s1 >> 19) | (s2 << 2);
+  s[5] = (s1 >> 19) | (s2 * (1L << 2));
   s[6] = s2 >> 6;
-  s[7] = (s2 >> 14) | (s3 << 7);
+  s[7] = (s2 >> 14) | (s3 * (1L << 7));
   s[8] = s3 >> 1;
   s[9] = s3 >> 9;
-  s[10] = (s3 >> 17) | (s4 << 4);
+  s[10] = (s3 >> 17) | (s4 * (1L << 4));
   s[11] = s4 >> 4;
   s[12] = s4 >> 12;
-  s[13] = (s4 >> 20) | (s5 << 1);
+  s[13] = (s4 >> 20) | (s5 * (1L << 1));
   s[14] = s5 >> 7;
-  s[15] = (s5 >> 15) | (s6 << 6);
+  s[15] = (s5 >> 15) | (s6 * (1L << 6));
   s[16] = s6 >> 2;
   s[17] = s6 >> 10;
-  s[18] = (s6 >> 18) | (s7 << 3);
+  s[18] = (s6 >> 18) | (s7 * (1L << 3));
   s[19] = s7 >> 5;
   s[20] = s7 >> 13;
   s[21] = s8 >> 0;
   s[22] = s8 >> 8;
-  s[23] = (s8 >> 16) | (s9 << 5);
+  s[23] = (s8 >> 16) | (s9 * (1L << 5));
   s[24] = s9 >> 3;
   s[25] = s9 >> 11;
-  s[26] = (s9 >> 19) | (s10 << 2);
+  s[26] = (s9 >> 19) | (s10 * (1L << 2));
   s[27] = s10 >> 6;
-  s[28] = (s10 >> 14) | (s11 << 7);
+  s[28] = (s10 >> 14) | (s11 * (1L << 7));
   s[29] = s11 >> 1;
   s[30] = s11 >> 9;
   s[31] = s11 >> 17;
@@ -2232,33 +2232,33 @@ void sc_reduce(unsigned char *s)
 
   s[0] = s0 >> 0;
   s[1] = s0 >> 8;
-  s[2] = (s0 >> 16) | (s1 << 5);
+  s[2] = (s0 >> 16) | (s1 * (1L << 5));
   s[3] = s1 >> 3;
   s[4] = s1 >> 11;
-  s[5] = (s1 >> 19) | (s2 << 2);
+  s[5] = (s1 >> 19) | (s2 * (1L << 2));
   s[6] = s2 >> 6;
-  s[7] = (s2 >> 14) | (s3 << 7);
+  s[7] = (s2 >> 14) | (s3 * (1L << 7));
   s[8] = s3 >> 1;
   s[9] = s3 >> 9;
-  s[10] = (s3 >> 17) | (s4 << 4);
+  s[10] = (s3 >> 17) | (s4 * (1L << 4));
   s[11] = s4 >> 4;
   s[12] = s4 >> 12;
-  s[13] = (s4 >> 20) | (s5 << 1);
+  s[13] = (s4 >> 20) | (s5 * (1L << 1));
   s[14] = s5 >> 7;
-  s[15] = (s5 >> 15) | (s6 << 6);
+  s[15] = (s5 >> 15) | (s6 * (1L << 6));
   s[16] = s6 >> 2;
   s[17] = s6 >> 10;
-  s[18] = (s6 >> 18) | (s7 << 3);
+  s[18] = (s6 >> 18) | (s7 * (1L << 3));
   s[19] = s7 >> 5;
   s[20] = s7 >> 13;
   s[21] = s8 >> 0;
   s[22] = s8 >> 8;
-  s[23] = (s8 >> 16) | (s9 << 5);
+  s[23] = (s8 >> 16) | (s9 * (1L << 5));
   s[24] = s9 >> 3;
   s[25] = s9 >> 11;
-  s[26] = (s9 >> 19) | (s10 << 2);
+  s[26] = (s9 >> 19) | (s10 * (1L << 2));
   s[27] = s10 >> 6;
-  s[28] = (s10 >> 14) | (s11 << 7);
+  s[28] = (s10 >> 14) | (s11 * (1L << 7));
   s[29] = s11 >> 1;
   s[30] = s11 >> 9;
   s[31] = s11 >> 17;
