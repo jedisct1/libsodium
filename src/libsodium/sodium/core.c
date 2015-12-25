35b0264cdd779ef79b051b077ab762a95a53b8ce
Frank Denis <github@pureftpd.org>
2015-12-25 11:58:08 +0100

    Get ready for the xmas release

--- src/libsodium/sodium/core.c
+++ src/libsodium/sodium/core.c
@@ -8,7 +8,7 @@
 #include "runtime.h"
 #include "utils.h"
 
-#if 1
+#if 0
 # warning This is unstable, untested, development code.
 # warning It might not compile. It might not work as expected.
 # warning It might be totally insecure.
