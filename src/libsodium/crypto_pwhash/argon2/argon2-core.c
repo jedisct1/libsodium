8b139cdc31ce5371e933fcd9d92c9d90c003db6e
Frank Denis <github@pureftpd.org>
2016-03-05 18:21:59 +0100

    Compile optimized Argon2i impl on 32-bit MSVC

--- src/libsodium/crypto_pwhash/argon2/argon2-core.c
+++ src/libsodium/crypto_pwhash/argon2/argon2-core.c
@@ -550,7 +550,7 @@ int initialize(argon2_instance_t *instance, argon2_context *context) {
 int argon2_pick_best_implementation(void)
 {
 #if (defined(HAVE_EMMINTRIN_H) && defined(HAVE_TMMINTRIN_H)) || \
-    (defined(_MSC_VER) && (defined(_M_X64) || defined(_M_AMD64)))
+    (defined(_MSC_VER) && (defined(_M_X64) || defined(_M_AMD64) || defined(_M_IX86)))
     if (sodium_runtime_has_ssse3()) {
         fill_segment = fill_segment_ssse3;
         return 0;
