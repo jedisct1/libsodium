f4397f12d0144a44c01d7fec5eab4548a5d0a947
Frank Denis <github@pureftpd.org>
2016-03-06 13:55:04 +0100

    Remove unneeded prototypes

--- src/libsodium/crypto_onetimeauth/poly1305/donna/poly1305_donna.h
+++ src/libsodium/crypto_onetimeauth/poly1305/donna/poly1305_donna.h
@@ -8,24 +8,4 @@
 extern struct crypto_onetimeauth_poly1305_implementation
     crypto_onetimeauth_poly1305_donna_implementation;
 
-static int crypto_onetimeauth_poly1305_donna(unsigned char *out,
-                                             const unsigned char *in,
-                                             unsigned long long inlen,
-                                             const unsigned char *k);
-
-static int crypto_onetimeauth_poly1305_donna_verify(const unsigned char *h,
-                                                    const unsigned char *in,
-                                                    unsigned long long inlen,
-                                                    const unsigned char *k);
-
-static int crypto_onetimeauth_poly1305_donna_init(crypto_onetimeauth_poly1305_state *state,
-                                                  const unsigned char *key);
-
-static int crypto_onetimeauth_poly1305_donna_update(crypto_onetimeauth_poly1305_state *state,
-                                                    const unsigned char *in,
-                                                    unsigned long long inlen);
-
-static int crypto_onetimeauth_poly1305_donna_final(crypto_onetimeauth_poly1305_state *state,
-                                                   unsigned char *out);
-
 #endif /* poly1305_donna_H */
