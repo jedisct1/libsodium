630ac0913bb8ebd967e2a3c195235e4de6a3d95d
Frank Denis <github@pureftpd.org>
2016-03-11 16:41:06 +0100

    We only support data independent addressing for Argon2 Let the compiler automatically remove unused code

--- src/libsodium/crypto_pwhash/argon2/argon2-fill-block-ref.c
+++ src/libsodium/crypto_pwhash/argon2/argon2-fill-block-ref.c
@@ -111,15 +111,13 @@ int fill_segment_ref(const argon2_instance_t *instance,
     uint32_t prev_offset, curr_offset;
     uint32_t starting_index;
     uint32_t i;
-    int data_independent_addressing;
+    const int data_independent_addressing = 1; /* instance->type == Argon2_i */
     /* Pseudo-random values that determine the reference block position */
     uint64_t *pseudo_rands = NULL;
 
     if (instance == NULL) {
         return ARGON2_OK;
     }
-
-    data_independent_addressing = (instance->type == Argon2_i);
 
     pseudo_rands =
         (uint64_t *)malloc(sizeof(uint64_t) * (instance->segment_length));
--- src/libsodium/crypto_pwhash/argon2/argon2-fill-block-ssse3.c
+++ src/libsodium/crypto_pwhash/argon2/argon2-fill-block-ssse3.c
@@ -105,7 +105,7 @@ int fill_segment_ssse3(const argon2_instance_t *instance,
     uint32_t prev_offset, curr_offset;
     uint32_t starting_index, i;
     __m128i state[64];
-    int data_independent_addressing;
+    const int data_independent_addressing = 1; /* instance->type == Argon2_i */
 
     /* Pseudo-random values that determine the reference block position */
     uint64_t *pseudo_rands = NULL;
@@ -113,8 +113,6 @@ int fill_segment_ssse3(const argon2_instance_t *instance,
     if (instance == NULL) {
         return ARGON2_OK;
     }
-
-    data_independent_addressing = (instance->type == Argon2_i);
 
     pseudo_rands =
         (uint64_t *)malloc(sizeof(uint64_t) * instance->segment_length);
