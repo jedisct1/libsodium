6c94f968e4924725b5a053b69232f25be2d74f2b
Frank Denis <github@pureftpd.org>
2016-03-18 09:59:32 +0100

    Remove BLAKE2s-related declarations

--- src/libsodium/crypto_generichash/blake2/ref/blake2.h
+++ src/libsodium/crypto_generichash/blake2/ref/blake2.h
@@ -38,15 +38,6 @@
 extern "C" {
 #endif
 
-  enum blake2s_constant
-  {
-    BLAKE2S_BLOCKBYTES = 64,
-    BLAKE2S_OUTBYTES   = 32,
-    BLAKE2S_KEYBYTES   = 32,
-    BLAKE2S_SALTBYTES  = 8,
-    BLAKE2S_PERSONALBYTES = 8
-  };
-
   enum blake2b_constant
   {
     BLAKE2B_BLOCKBYTES = 128,
@@ -61,30 +52,6 @@ extern "C" {
 #else
 # pragma pack(push, 1)
 #endif
-
-  typedef struct blake2s_param_
-  {
-    uint8_t  digest_length;  /*  1 */
-    uint8_t  key_length;     /*  2 */
-    uint8_t  fanout;         /*  3 */
-    uint8_t  depth;          /*  4 */
-    uint8_t  leaf_length[4]; /*  8 */
-    uint8_t  node_offset[6]; /* 14 */
-    uint8_t  node_depth;     /* 15 */
-    uint8_t  inner_length;   /* 16 */
-    uint8_t  salt[BLAKE2S_SALTBYTES]; /* 24 */
-    uint8_t  personal[BLAKE2S_PERSONALBYTES]; /* 32 */
-  } blake2s_param;
-
-CRYPTO_ALIGN( 64 ) typedef struct blake2s_state_
-  {
-    uint32_t h[8];
-    uint32_t t[2];
-    uint32_t f[2];
-    uint8_t  buf[2 * BLAKE2S_BLOCKBYTES];
-    size_t   buflen;
-    uint8_t  last_node;
-  } blake2s_state ;
 
   typedef struct blake2b_param_
   {
@@ -115,14 +82,6 @@ CRYPTO_ALIGN( 64 ) typedef struct blake2b_state_
   } blake2b_state;
 #endif
 
-  typedef struct blake2sp_state_
-  {
-    blake2s_state S[8][1];
-    blake2s_state R[1];
-    uint8_t buf[8 * BLAKE2S_BLOCKBYTES];
-    size_t  buflen;
-  } blake2sp_state;
-
   typedef struct blake2bp_state_
   {
     blake2b_state S[4][1];
@@ -138,12 +97,6 @@ CRYPTO_ALIGN( 64 ) typedef struct blake2b_state_
 #endif
 
   /* Streaming API */
-  int blake2s_init( blake2s_state *S, const uint8_t outlen );
-  int blake2s_init_key( blake2s_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
-  int blake2s_init_param( blake2s_state *S, const blake2s_param *P );
-  int blake2s_update( blake2s_state *S, const uint8_t *in, uint64_t inlen );
-  int blake2s_final( blake2s_state *S, uint8_t *out, uint8_t outlen );
-
   int blake2b_init( blake2b_state *S, const uint8_t outlen );
   int blake2b_init_salt_personal( blake2b_state *S, const uint8_t outlen,
                                   const void *personal, const void *salt );
@@ -154,23 +107,14 @@ CRYPTO_ALIGN( 64 ) typedef struct blake2b_state_
   int blake2b_update( blake2b_state *S, const uint8_t *in, uint64_t inlen );
   int blake2b_final( blake2b_state *S, uint8_t *out, uint8_t outlen );
 
-  int blake2sp_init( blake2sp_state *S, const uint8_t outlen );
-  int blake2sp_init_key( blake2sp_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
-  int blake2sp_update( blake2sp_state *S, const uint8_t *in, uint64_t inlen );
-  int blake2sp_final( blake2sp_state *S, uint8_t *out, uint8_t outlen );
-
   int blake2bp_init( blake2bp_state *S, const uint8_t outlen );
   int blake2bp_init_key( blake2bp_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
   int blake2bp_update( blake2bp_state *S, const uint8_t *in, uint64_t inlen );
   int blake2bp_final( blake2bp_state *S, uint8_t *out, uint8_t outlen );
 
   /* Simple API */
-  int blake2s( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
   int blake2b( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
   int blake2b_salt_personal( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen, const void *salt, const void *personal );
-
-  int blake2sp( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
-  int blake2bp( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
 
   static inline int blake2( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen )
   {
