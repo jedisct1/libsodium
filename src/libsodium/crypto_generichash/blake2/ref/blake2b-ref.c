0c069792607179d7c61e85400106bb5d9ecce24b
Frank Denis <github@pureftpd.org>
2016-03-18 10:20:56 +0100

    Verify at compile time that blake2b_param is packed as expected

--- src/libsodium/crypto_generichash/blake2/ref/blake2b-ref.c
+++ src/libsodium/crypto_generichash/blake2/ref/blake2b-ref.c
@@ -156,6 +156,7 @@ int blake2b_init_param( blake2b_state *S, const blake2b_param *P )
   size_t i;
   const uint8_t *p;
 
+  (void) sizeof(int[sizeof *P == 64 ? 1 : -1]);
   blake2b_init0( S );
   p = ( const uint8_t * )( P );
 
