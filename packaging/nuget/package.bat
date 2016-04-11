5b3c23d68c534c46019b3a838cb4dd575b05a5a3
Frank Denis <github@pureftpd.org>
2016-04-12 01:27:22 +0200

    https

--- packaging/nuget/package.bat
+++ packaging/nuget/package.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 ECHO Started nuget packaging build.
 ECHO.
-REM http://www.nuget.org/packages/gsl
+REM https://www.nuget.org/packages/gsl
 gsl -q -script:package.gsl package.config
 ECHO.
-REM http://nuget.codeplex.com/releases
+REM https://nuget.codeplex.com/releases
 nuget pack package.nuspec -verbosity detailed
 ECHO.
 ECHO NOTE: Ignore warnings not applicable to native code: "Issue: Assembly outside lib folder."
