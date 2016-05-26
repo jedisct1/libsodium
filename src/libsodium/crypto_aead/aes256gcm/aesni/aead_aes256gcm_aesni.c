145a662a10f9f4abe35dd44b57dab7ac29277928
Frank Denis <github@pureftpd.org>
2016-05-26 06:08:26 -0700

    Don't include <immintrin.h> if it is not needed
    Some environments provide <wmmintrin.h> but not <immintrin.h>

--- src/libsodium/crypto_aead/aes256gcm/aesni/aead_aes256gcm_aesni.c
+++ src/libsodium/crypto_aead/aes256gcm/aesni/aead_aes256gcm_aesni.c
@@ -13,14 +13,15 @@
 #include "runtime.h"
 #include "utils.h"
 
-#if defined(HAVE_WMMINTRIN_H) || \
+#if (defined(HAVE_TMMINTRIN_H) && defined(HAVE_WMMINTRIN_H)) || \
     (defined(_MSC_VER) && _MSC_VER >= 1600 && (defined(_M_X64) || defined(_M_AMD64) || defined(_M_IX86)))
 
 #pragma GCC target("ssse3")
 #pragma GCC target("aes")
 #pragma GCC target("pclmul")
 
-#include <immintrin.h>
+#include <tmmintrin.h>
+#include <wmmintrin.h>
 
 #ifndef ENOSYS
 # define ENOSYS ENXIO
