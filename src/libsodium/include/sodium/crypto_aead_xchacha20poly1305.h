1686da3d3cf1d3c40a6985f8c515d20feb451bf8
Frank Denis <github@pureftpd.org>
2017-01-13 19:24:48 +0100

    Remove the non-IETF versions of crypto_aead_xchacha20poly1305

--- src/libsodium/include/sodium/crypto_aead_xchacha20poly1305.h
+++ src/libsodium/include/sodium/crypto_aead_xchacha20poly1305.h
@@ -11,22 +11,6 @@
 extern "C" {
 #endif
 
-#define crypto_aead_xchacha20poly1305_KEYBYTES 32U
-SODIUM_EXPORT
-size_t crypto_aead_xchacha20poly1305_keybytes(void);
-
-#define crypto_aead_xchacha20poly1305_NSECBYTES 0U
-SODIUM_EXPORT
-size_t crypto_aead_xchacha20poly1305_nsecbytes(void);
-
-#define crypto_aead_xchacha20poly1305_NPUBBYTES 24U
-SODIUM_EXPORT
-size_t crypto_aead_xchacha20poly1305_npubbytes(void);
-
-#define crypto_aead_xchacha20poly1305_ABYTES 16U
-SODIUM_EXPORT
-size_t crypto_aead_xchacha20poly1305_abytes(void);
-
 #define crypto_aead_xchacha20poly1305_ietf_KEYBYTES 32U
 SODIUM_EXPORT
 size_t crypto_aead_xchacha20poly1305_ietf_keybytes(void);
@@ -42,53 +26,6 @@ size_t crypto_aead_xchacha20poly1305_ietf_npubbytes(void);
 #define crypto_aead_xchacha20poly1305_ietf_ABYTES 16U
 SODIUM_EXPORT
 size_t crypto_aead_xchacha20poly1305_ietf_abytes(void);
-
-SODIUM_EXPORT
-int crypto_aead_xchacha20poly1305_encrypt(unsigned char *c,
-                                          unsigned long long *clen_p,
-                                          const unsigned char *m,
-                                          unsigned long long mlen,
-                                          const unsigned char *ad,
-                                          unsigned long long adlen,
-                                          const unsigned char *nsec,
-                                          const unsigned char *npub,
-                                          const unsigned char *k);
-
-SODIUM_EXPORT
-int crypto_aead_xchacha20poly1305_decrypt(unsigned char *m,
-                                          unsigned long long *mlen_p,
-                                          unsigned char *nsec,
-                                          const unsigned char *c,
-                                          unsigned long long clen,
-                                          const unsigned char *ad,
-                                          unsigned long long adlen,
-                                          const unsigned char *npub,
-                                          const unsigned char *k)
-            __attribute__ ((warn_unused_result));
-
-SODIUM_EXPORT
-int crypto_aead_xchacha20poly1305_encrypt_detached(unsigned char *c,
-                                                   unsigned char *mac,
-                                                   unsigned long long *maclen_p,
-                                                   const unsigned char *m,
-                                                   unsigned long long mlen,
-                                                   const unsigned char *ad,
-                                                   unsigned long long adlen,
-                                                   const unsigned char *nsec,
-                                                   const unsigned char *npub,
-                                                   const unsigned char *k);
-
-SODIUM_EXPORT
-int crypto_aead_xchacha20poly1305_decrypt_detached(unsigned char *m,
-                                                   unsigned char *nsec,
-                                                   const unsigned char *c,
-                                                   unsigned long long clen,
-                                                   const unsigned char *mac,
-                                                   const unsigned char *ad,
-                                                   unsigned long long adlen,
-                                                   const unsigned char *npub,
-                                                   const unsigned char *k)
-        __attribute__ ((warn_unused_result));
 
 SODIUM_EXPORT
 int crypto_aead_xchacha20poly1305_ietf_encrypt(unsigned char *c,
