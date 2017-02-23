d5bf3954d63775db7be8dc760e4b084ca4fc1544
Frank Denis <github@pureftpd.org>
2017-02-23 11:25:09 +0100

    Indentx

--- test/default/hash.c
+++ test/default/hash.c
@@ -3,31 +3,35 @@
 #include "cmptest.h"
 
 static unsigned char x[] = "testing\n";
-static unsigned char x2[] = "The Conscience of a Hacker is a small essay written January 8, 1986 by a computer security hacker who went by the handle of The Mentor, who belonged to the 2nd generation of Legion of Doom.";
+static unsigned char x2[] =
+    "The Conscience of a Hacker is a small essay written January 8, 1986 by a "
+    "computer security hacker who went by the handle of The Mentor, who "
+    "belonged to the 2nd generation of Legion of Doom.";
 static unsigned char h[crypto_hash_BYTES];
 
-int main(void)
+int
+main(void)
 {
     size_t i;
 
     crypto_hash(h, x, sizeof x - 1U);
     for (i = 0; i < crypto_hash_BYTES; ++i) {
-        printf("%02x", (unsigned int)h[i]);
+        printf("%02x", (unsigned int) h[i]);
     }
     printf("\n");
     crypto_hash(h, x2, sizeof x2 - 1U);
     for (i = 0; i < crypto_hash_BYTES; ++i) {
-        printf("%02x", (unsigned int)h[i]);
+        printf("%02x", (unsigned int) h[i]);
     }
     printf("\n");
     crypto_hash_sha256(h, x, sizeof x - 1U);
     for (i = 0; i < crypto_hash_sha256_BYTES; ++i) {
-        printf("%02x", (unsigned int)h[i]);
+        printf("%02x", (unsigned int) h[i]);
     }
     printf("\n");
     crypto_hash_sha256(h, x2, sizeof x2 - 1U);
     for (i = 0; i < crypto_hash_sha256_BYTES; ++i) {
-        printf("%02x", (unsigned int)h[i]);
+        printf("%02x", (unsigned int) h[i]);
     }
     printf("\n");
 
--- test/default/hash3.c
+++ test/default/hash3.c
@@ -5,13 +5,14 @@
 static unsigned char x[] = "testing\n";
 static unsigned char h[crypto_hash_BYTES];
 
-int main(void)
+int
+main(void)
 {
     size_t i;
 
     crypto_hash(h, x, sizeof x - 1U);
     for (i = 0; i < crypto_hash_BYTES; ++i) {
-        printf("%02x", (unsigned int)h[i]);
+        printf("%02x", (unsigned int) h[i]);
     }
     printf("\n");
 
