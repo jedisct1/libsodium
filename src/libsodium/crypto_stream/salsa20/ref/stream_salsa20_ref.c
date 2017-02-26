e4a43d5cff7ac0c139d4201fe660f514099ebbad
Frank Denis <github@pureftpd.org>
2017-02-26 17:35:30 +0100

    Nits

--- src/libsodium/crypto_stream/salsa20/ref/stream_salsa20_ref.c
+++ src/libsodium/crypto_stream/salsa20/ref/stream_salsa20_ref.c
@@ -10,11 +10,14 @@ Public domain.
 #include "crypto_stream_salsa20.h"
 #include "utils.h"
 
+#include "../stream_salsa20.h"
+#include "stream_salsa20_ref.h"
+
 #if !(defined(HAVE_EMMINTRIN_H) && defined(__x86_64__))
 
 static int
-stream_ref(unsigned char *c, unsigned long long clen,
-           const unsigned char *n, const unsigned char *k)
+stream_ref(unsigned char *c, unsigned long long clen, const unsigned char *n,
+           const unsigned char *k)
 {
     unsigned char in[16];
     unsigned char block[64];
@@ -25,20 +28,20 @@ stream_ref(unsigned char *c, unsigned long long clen,
     if (!clen) {
         return 0;
     }
-    for (i = 0; i < 32; ++i) {
+    for (i = 0; i < 32; i++) {
         kcopy[i] = k[i];
     }
-    for (i = 0; i < 8; ++i) {
+    for (i = 0; i < 8; i++) {
         in[i] = n[i];
     }
-    for (i = 8; i < 16; ++i) {
+    for (i = 8; i < 16; i++) {
         in[i] = 0;
     }
     while (clen >= 64) {
         crypto_core_salsa20(c, in, kcopy, NULL);
         u = 1;
-        for (i = 8; i < 16; ++i) {
-            u += (unsigned int)in[i];
+        for (i = 8; i < 16; i++) {
+            u += (unsigned int) in[i];
             in[i] = u;
             u >>= 8;
         }
@@ -47,7 +50,7 @@ stream_ref(unsigned char *c, unsigned long long clen,
     }
     if (clen) {
         crypto_core_salsa20(block, in, kcopy, NULL);
-        for (i = 0; i < (unsigned int)clen; ++i) {
+        for (i = 0; i < (unsigned int) clen; i++) {
             c[i] = block[i];
         }
     }
@@ -59,8 +62,8 @@ stream_ref(unsigned char *c, unsigned long long clen,
 
 static int
 stream_ref_xor_ic(unsigned char *c, const unsigned char *m,
-                  unsigned long long mlen, const unsigned char *n,
-                  uint64_t ic, const unsigned char *k)
+                  unsigned long long mlen, const unsigned char *n, uint64_t ic,
+                  const unsigned char *k)
 {
     unsigned char in[16];
     unsigned char block[64];
@@ -71,24 +74,24 @@ stream_ref_xor_ic(unsigned char *c, const unsigned char *m,
     if (!mlen) {
         return 0;
     }
-    for (i = 0; i < 32; ++i) {
+    for (i = 0; i < 32; i++) {
         kcopy[i] = k[i];
     }
-    for (i = 0; i < 8; ++i) {
+    for (i = 0; i < 8; i++) {
         in[i] = n[i];
     }
-    for (i = 8; i < 16; ++i) {
-        in[i] = (unsigned char)(ic & 0xff);
+    for (i = 8; i < 16; i++) {
+        in[i] = (unsigned char) (ic & 0xff);
         ic >>= 8;
     }
     while (mlen >= 64) {
         crypto_core_salsa20(block, in, kcopy, NULL);
-        for (i = 0; i < 64; ++i) {
+        for (i = 0; i < 64; i++) {
             c[i] = m[i] ^ block[i];
         }
         u = 1;
-        for (i = 8; i < 16; ++i) {
-            u += (unsigned int)in[i];
+        for (i = 8; i < 16; i++) {
+            u += (unsigned int) in[i];
             in[i] = u;
             u >>= 8;
         }
@@ -98,7 +101,7 @@ stream_ref_xor_ic(unsigned char *c, const unsigned char *m,
     }
     if (mlen) {
         crypto_core_salsa20(block, in, kcopy, NULL);
-        for (i = 0; i < (unsigned int)mlen; ++i) {
+        for (i = 0; i < (unsigned int) mlen; i++) {
             c[i] = m[i] ^ block[i];
         }
     }
