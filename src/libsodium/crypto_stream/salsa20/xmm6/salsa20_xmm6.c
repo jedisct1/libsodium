e05d2b3d8f45e0eb520c6cc27b155d8953c7c5a5
Frank Denis <github@pureftpd.org>
2017-02-27 00:36:59 +0100

    C++ compat

--- src/libsodium/crypto_stream/salsa20/xmm6/salsa20_xmm6.c
+++ src/libsodium/crypto_stream/salsa20/xmm6/salsa20_xmm6.c
@@ -8,6 +8,9 @@
 
 #ifdef HAVE_AMD64_ASM
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern int stream_salsa20_xmm6(unsigned char *c, unsigned long long clen,
                                const unsigned char *n, const unsigned char *k);
 
@@ -15,6 +18,9 @@ extern int stream_salsa20_xmm6_xor_ic(unsigned char *c, const unsigned char *m,
                                       unsigned long long mlen,
                                       const unsigned char *n,
                                       uint64_t ic, const unsigned char *k);
+#ifdef __cplusplus
+}
+#endif
 
 struct crypto_stream_salsa20_implementation
     crypto_stream_salsa20_xmm6_implementation = {
