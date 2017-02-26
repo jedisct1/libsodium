7d29c0fbd775f74b9cc5c03428f33f7f3372c791
Frank Denis <github@pureftpd.org>
2017-02-26 23:51:58 +0100

    Remove useless prototypes

--- src/libsodium/crypto_stream/salsa20/ref/salsa20_ref.h
+++ src/libsodium/crypto_stream/salsa20/ref/salsa20_ref.h
@@ -6,11 +6,3 @@
 
 extern struct crypto_stream_salsa20_implementation
     crypto_stream_salsa20_ref_implementation;
-
-int crypto_stream_salsa20_ref(unsigned char *c, unsigned long long clen,
-                              const unsigned char *n, const unsigned char *k);
-
-int crypto_stream_salsa20_ref_xor_ic(unsigned char *c, const unsigned char *m,
-                                     unsigned long long   mlen,
-                                     const unsigned char *n, uint64_t ic,
-                                     const unsigned char *k);
