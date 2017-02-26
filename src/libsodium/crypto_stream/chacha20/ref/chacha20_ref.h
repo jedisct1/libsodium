8a213f021cea94902add6c748d1f060f6bd6289b
Frank Denis <github@pureftpd.org>
2017-02-26 21:26:14 +0100

    Remove useless prototypes

--- src/libsodium/crypto_stream/chacha20/ref/chacha20_ref.h
+++ src/libsodium/crypto_stream/chacha20/ref/chacha20_ref.h
@@ -6,21 +6,3 @@
 
 extern struct crypto_stream_chacha20_implementation
     crypto_stream_chacha20_ref_implementation;
-
-int crypto_stream_chacha20_ref(unsigned char *c, unsigned long long clen,
-                               const unsigned char *n, const unsigned char *k);
-
-int crypto_stream_chacha20_ref_xor_ic(unsigned char *c, const unsigned char *m,
-                                      unsigned long long   mlen,
-                                      const unsigned char *n, uint64_t ic,
-                                      const unsigned char *k);
-
-int crypto_stream_chacha20_ietf_ref(unsigned char *c, unsigned long long clen,
-                                    const unsigned char *n,
-                                    const unsigned char *k);
-
-int crypto_stream_chacha20_ietf_ref_xor_ic(unsigned char *      c,
-                                           const unsigned char *m,
-                                           unsigned long long   mlen,
-                                           const unsigned char *n, uint32_t ic,
-                                           const unsigned char *k);
