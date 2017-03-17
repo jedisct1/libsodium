c1ab6f23284785c998c970b4b7a9b9c0c8d94642
Frank Denis <github@pureftpd.org>
2017-03-17 16:56:20 +0100

    emscripten: don't use --enable-minimal on sumo builds

--- dist-build/emscripten.sh
+++ dist-build/emscripten.sh
@@ -20,6 +20,7 @@ if [ "x$1" = "x--standard" ]; then
   export LDFLAGS="${LDFLAGS} ${LDFLAGS_DIST} -s TOTAL_MEMORY=${TOTAL_MEMORY}"
   export PREFIX="$(pwd)/libsodium-js"
   export DONE_FILE="$(pwd)/js.done"
+  export CONFIG_EXTRA="--enable-minimal"
   export DIST='yes'
 elif [ "x$1" = "x--sumo" ]; then
   echo "Building a sumo distribution in ${PREFIX}"
@@ -54,7 +55,8 @@ export JS_EXPORTS_FLAGS="-s EXPORTED_FUNCTIONS=${EXPORTED_FUNCTIONS}"
 rm -f "$DONE_FILE"
 
 echo
-emconfigure ./configure --enable-minimal --disable-shared --prefix="$PREFIX" \
+
+emconfigure ./configure $CONFIG_EXTRA --disable-shared --prefix="$PREFIX" \
                         --without-pthreads CFLAGS="$CFLAGS" && \
 emmake make clean
 [ $? = 0 ] || exit 1
