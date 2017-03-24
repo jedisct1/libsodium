415bc429a76619717f44650922afdf47b12da789
Frank Denis <github@pureftpd.org>
2017-03-24 17:21:51 +0100

    Android: the mipsel target that disappeared in r14 is back in r14b

--- dist-build/android-mips32.sh
+++ dist-build/android-mips32.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export TARGET_ARCH=mips32
 export CFLAGS="-Os"
-ARCH=mips HOST_COMPILER=mips64el-linux-android "$(dirname "$0")/android-build.sh"
+ARCH=mips HOST_COMPILER=mipsel-linux-android "$(dirname "$0")/android-build.sh"
