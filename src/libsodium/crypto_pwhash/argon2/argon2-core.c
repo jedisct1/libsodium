935557cdf7defe9d29861264c758a77a60ef9b38
Frank Denis <github@pureftpd.org>
2017-06-27 14:19:23 +0200

    Add AVX2-optimized Argon2 blamka function

--- src/libsodium/crypto_pwhash/argon2/argon2-core.c
+++ src/libsodium/crypto_pwhash/argon2/argon2-core.c
@@ -591,6 +591,13 @@ int
 argon2_pick_best_implementation(void)
 {
 /* LCOV_EXCL_START */
+#if defined(HAVE_AVX2INTRIN_H) && defined(HAVE_TMMINTRIN_H) && \
+    defined(HAVE_SMMINTRIN_H)
+    if (sodium_runtime_has_avx2()) {
+        fill_segment = fill_segment_avx2;
+        return 0;
+    }
+#endif
 #if defined(HAVE_EMMINTRIN_H) && defined(HAVE_TMMINTRIN_H)
     if (sodium_runtime_has_ssse3()) {
         fill_segment = fill_segment_ssse3;
