77b1d8eeb3379c4ee2fd7ad202aa5f3a4df26947
Frank Denis <github@pureftpd.org>
2017-06-27 16:18:55 +0200

    implementations.h doesn't need C++ guards

--- src/libsodium/include/sodium/private/implementations.h
+++ src/libsodium/include/sodium/private/implementations.h
@@ -1,19 +1,6 @@
 #ifndef implementations_H
 #define implementations_H
 
-#include "export.h"
-
-#ifdef __cplusplus
-# ifdef __GNUC__
-#  pragma GCC diagnostic ignored "-Wlong-long"
-# endif
-extern "C" {
-#endif
-
 int _crypto_pwhash_argon2_pick_best_implementation(void);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
