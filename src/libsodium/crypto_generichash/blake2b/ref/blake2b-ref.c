1090fcfd4dd3ca1b3abf774efcf0698b4f3799b2
Frank Denis <github@pureftpd.org>
2017-07-18 19:19:04 +0200

    memzero() the state if we call generichash_final() twice

--- src/libsodium/crypto_generichash/blake2b/ref/blake2b-ref.c
+++ src/libsodium/crypto_generichash/blake2b/ref/blake2b-ref.c
@@ -359,6 +359,7 @@ blake2b_final(blake2b_state *S, uint8_t *out, uint8_t outlen)
         sodium_misuse("blake2b_final(): unsupported output length");
     }
     if (blake2b_is_lastblock(S)) {
+        sodium_memzero(S, sizeof *S);
         return -1;
     }
     if (S->buflen > BLAKE2B_BLOCKBYTES) {
