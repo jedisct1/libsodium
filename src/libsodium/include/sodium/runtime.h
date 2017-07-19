cc51916072f645e0ff6528f515d9de17655929de
Frank Denis <github@pureftpd.org>
2017-07-19 12:30:40 +0200

    Tag sodium_runtime_has_*() symbols as weak

--- src/libsodium/include/sodium/runtime.h
+++ src/libsodium/include/sodium/runtime.h
@@ -8,31 +8,31 @@
 extern "C" {
 #endif
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_neon(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_sse2(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_sse3(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_ssse3(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_sse41(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_avx(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_avx2(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_pclmul(void);
 
-SODIUM_EXPORT
+SODIUM_EXPORT __attribute__((weak))
 int sodium_runtime_has_aesni(void);
 
 /* ------------------------------------------------------------------------- */
