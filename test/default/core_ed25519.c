1df228536251ef66fd5998e4a1036019200a0796
Frank Denis <github@pureftpd.org>
2017-11-15 01:34:43 +0100

    Add a preliminary test for core_ed25519

---
+++ test/default/core_ed25519.c
@@ -0,0 +1,75 @@
+
+#define TEST_NAME "core_ed25519"
+#include "cmptest.h"
+
+int
+main(void)
+{
+    unsigned char *h;
+    unsigned char *p, *p2, *p3;
+    unsigned char *sc;
+    int            i, j;
+
+    h = sodium_malloc(crypto_core_ed25519_UNIFORMBYTES);
+    p = sodium_malloc(crypto_core_ed25519_BYTES);
+    for (i = 0; i < 1000; i++) {
+        randombytes_buf(h, crypto_core_ed25519_UNIFORMBYTES);
+        if (crypto_core_ed25519_from_uniform(p, h) != 0) {
+            printf("crypto_core_ed25519_from_uniform() failed\n");
+        }
+        if (crypto_core_ed25519_is_valid_point(p) == 0) {
+            printf("crypto_core_ed25519_from_uniform() returned an invalid point\n");
+        }
+    }
+
+    p2 = sodium_malloc(crypto_core_ed25519_BYTES);
+    p3 = sodium_malloc(crypto_core_ed25519_BYTES);
+    randombytes_buf(h, crypto_core_ed25519_UNIFORMBYTES);
+    crypto_core_ed25519_from_uniform(p2, h);
+
+    j = 1 + (int) randombytes_uniform(100);
+    memcpy(p3, p, crypto_core_ed25519_BYTES);
+    for (i = 0; i < j; i++) {
+        crypto_core_ed25519_add(p, p, p2);
+        if (crypto_core_ed25519_is_valid_point(p) != 1) {
+            printf("crypto_core_add() returned an invalid point\n");
+        }
+    }
+    if (memcmp(p, p3, crypto_core_ed25519_BYTES) == 0) {
+        printf("crypto_core_add() failed\n");
+    }
+    for (i = 0; i < j; i++) {
+        crypto_core_ed25519_sub(p, p, p2);
+    }
+    if (memcmp(p, p3, crypto_core_ed25519_BYTES) != 0) {
+        printf("crypto_core_add() or crypto_core_sub() failed\n");
+    }
+    sc = sodium_malloc(crypto_scalarmult_ed25519_SCALARBYTES);
+    memset(sc, 0, crypto_scalarmult_ed25519_SCALARBYTES);
+    sc[0] = 8;
+    memcpy(p2, p, crypto_core_ed25519_BYTES);
+    memcpy(p3, p, crypto_core_ed25519_BYTES);
+
+    for (i = 0; i < 254; i++) {
+        crypto_core_ed25519_add(p2, p2, p2);
+    }
+    for (i = 0; i < 8; i++) {
+        crypto_core_ed25519_add(p2, p2, p);
+    }
+    if (crypto_scalarmult_ed25519(p3, sc, p) != 0) {
+        printf("crypto_scalarmult_ed25519() failed\n");
+    }
+    if (memcmp(p2, p3, crypto_core_ed25519_BYTES) != 0) {
+        printf("crypto_scalarmult_ed25519() is inconsistent with crypto_core_ed25519_add()\n");
+    }
+
+    sodium_free(sc);
+    sodium_free(p3);
+    sodium_free(p2);
+    sodium_free(p);
+    sodium_free(h);
+
+    printf("OK\n");
+
+    return 0;
+}
