2f564436315bf9264577dcb0a3a2c0ff6c076c35
Frank Denis <github@pureftpd.org>
2017-12-12 22:23:37 +0100

    Don't redefine alloca

--- src/libsodium/sodium/utils.c
+++ src/libsodium/sodium/utils.c
@@ -23,19 +23,21 @@
 
 #if HAVE_ALLOCA_H
 # include <alloca.h>
-#elif defined __GNUC__
-# define alloca __builtin_alloca
-#elif defined _AIX
-# define alloca __alloca
-#elif defined _MSC_VER
-# include <malloc.h>
-# define alloca _alloca
-#else
-# include <stddef.h>
-# ifdef  __cplusplus
+#elif !defined(alloca)
+# if defined(__GNUC__)
+#  define alloca __builtin_alloca
+# elif defined _AIX
+#  define alloca __alloca
+# elif defined _MSC_VER
+#  include <malloc.h>
+#  define alloca _alloca
+# else
+#  include <stddef.h>
+#  ifdef  __cplusplus
 extern "C"
-# endif
+#  endif
 void *alloca (size_t);
+# endif
 #endif
 
 #include "core.h"
