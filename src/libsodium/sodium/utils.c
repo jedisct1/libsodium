764656443fa6d6d7f609945e1d7579536e45508b
Frank Denis <github@pureftpd.org>
2017-12-31 01:23:58 +0100

    Check if we can use inline asm code, not only on x86_64

--- src/libsodium/sodium/utils.c
+++ src/libsodium/sodium/utils.c
@@ -113,7 +113,7 @@ sodium_memzero(void *const pnt, const size_t len)
 #elif HAVE_WEAK_SYMBOLS
     memset(pnt, 0, len);
     _sodium_dummy_symbol_to_prevent_memzero_lto(pnt, len);
-# ifdef HAVE_AMD64_ASM
+# ifdef HAVE_INLINE_ASM
     __asm__ __volatile__ ("" : : "r"(pnt) : "memory");
 # endif
 #else
