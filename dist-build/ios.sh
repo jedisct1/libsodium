2176aae7e4a57109bb978f4ee74f645db8b6de1a
Daniel Persson <mailto.woden@gmail.com>
2018-03-06 21:10:05 +0100

    Change so we set depending on a full build flag.

--- dist-build/ios.sh
+++ dist-build/ios.sh
@@ -40,9 +40,9 @@ export LDFLAGS="-arch i386 -isysroot ${SDK} -mios-simulator-version-min=${IOS_SI
 make distclean > /dev/null
 
 if [ -z "$LIBSODIUM_FULL_BUILD" ]; then
-  export LIBSODIUM_ENABLE_MINIMAL_FLAG="--enable-minimal"
-else
   export LIBSODIUM_ENABLE_MINIMAL_FLAG=""
+else
+  export LIBSODIUM_ENABLE_MINIMAL_FLAG="--enable-minimal"
 fi
 
 ./configure --host=i686-apple-darwin10 \
