41d65f88370fc171ccf7134d02c4e46ad08fe04f
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-06-10 16:40:40 +0200

    Replace sodium_malloc by a memory pool for enclave tests. sodium_malloc (and most of the memory management functions of utils.h) use syscalls, are not supported inside an enclave (it raises a SIGILL), which means that, to run the tests inside an enclave, we must allocate the memory otherwise. I just reused the code of cmptest.h in benchmark mode. Also, the sodium_utils3 test is now disabled during an SGX build.

--- test/default/test_enclave.h
+++ test/default/test_enclave.h
@@ -59,6 +59,83 @@ int rand_sgx()
     return (ret>=0) ? ret : -ret;
 }
 
+
+struct {
+    void   *pnt;
+    size_t  size;
+} mempool[1024];
+
+static size_t mempool_idx;
+
+static __attribute__((malloc)) void *mempool_alloc(size_t size)
+{
+    size_t i;
+    if (size >= (size_t) 0x80000000 - (size_t) 0x00000fff) {
+        return NULL;
+    }
+    size = (size + (size_t) 0x00000fff) & ~ (size_t) 0x00000fff;
+    for (i = 0U; i < mempool_idx; i++) {
+        if (mempool[i].size >= (size | (size_t) 0x80000000)) {
+            mempool[i].size &= ~ (size_t) 0x80000000;
+            return mempool[i].pnt;
+        }
+    }
+    if (mempool_idx >= sizeof mempool / sizeof mempool[0]) {
+        return NULL;
+    }
+    mempool[mempool_idx].size = size;
+    return (mempool[mempool_idx++].pnt = (void *) malloc(size));
+}
+
+static void mempool_free(void *pnt)
+{
+    size_t i;
+    for (i = 0U; i < mempool_idx; i++) {
+        if (mempool[i].pnt == pnt) {
+            if ((mempool[i].size & (size_t) 0x80000000) != (size_t) 0x0) {
+                break;
+            }
+            mempool[i].size |= (size_t) 0x80000000;
+            return;
+        }
+    }
+    abort();
+}
+
+static __attribute__((malloc)) void *mempool_allocarray(size_t count, size_t size)
+{
+    if (count > (size_t) 0U && size >= (size_t) SIZE_MAX / count) {
+        return NULL;
+    }
+    return mempool_alloc(count * size);
+}
+
+static int mempool_free_all(void)
+{
+    size_t i;
+    int    ret = 0;
+
+    for (i = 0U; i < mempool_idx; i++) {
+        if ((mempool[i].size & (size_t) 0x80000000) == (size_t) 0x0) {
+            ret = -1;
+        }
+        free(mempool[i].pnt);
+        mempool[i].pnt = NULL;
+    }
+    mempool_idx = (size_t) 0U;
+
+    return ret;
+}
+
+#undef sodium_malloc 
+#undef sodium_free
+#undef sodium_allocarray
+
+#define sodium_malloc(X)        mempool_alloc(X)
+#define sodium_free(X)          mempool_free(X)
+#define sodium_allocarray(X, Y) mempool_allocarray((X), (Y))
+
+
 int xmain(void);
 
 void run_test(int* return_code)
@@ -76,6 +153,11 @@ void run_test(int* return_code)
             *return_code = 99;
         }else{
             log_string("Enclave: test succeeded.\n");
+        }
+        
+        if (mempool_free_all() != 0) {
+            log_string("** Enclave: memory leaks detected **\n");
+            *return_code = 99;
         }
     }
 }
