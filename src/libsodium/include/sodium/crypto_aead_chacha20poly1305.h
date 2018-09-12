90112d3eb0aba74ef3b032ed3d40acf71a806e04
Frank Denis <github@pureftpd.org>
2018-09-12 15:19:56 +0200

    Substract the number of blocks, and make similar code more uniform

--- src/libsodium/include/sodium/crypto_aead_chacha20poly1305.h
+++ src/libsodium/include/sodium/crypto_aead_chacha20poly1305.h
@@ -32,7 +32,7 @@ size_t crypto_aead_chacha20poly1305_ietf_abytes(void);
 
 #define crypto_aead_chacha20poly1305_ietf_MESSAGEBYTES_MAX \
     SODIUM_MIN(SODIUM_SIZE_MAX - crypto_aead_chacha20poly1305_ietf_ABYTES, \
-               (64ULL * (1ULL << 32) - 64ULL) - crypto_aead_chacha20poly1305_ietf_ABYTES)
+               (64ULL * ((1ULL << 32) - 1ULL)) - crypto_aead_chacha20poly1305_ietf_ABYTES)
 SODIUM_EXPORT
 size_t crypto_aead_chacha20poly1305_ietf_messagebytes_max(void);
 
