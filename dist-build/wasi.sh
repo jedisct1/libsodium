8745c8511471d11013d9da68c245f3f58b90493c
Frank Denis <github@pureftpd.org>
2019-04-08 20:47:33 +0200

    First step towards WASI support

---
+++ dist-build/wasi.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+if [ -z "$WASI_SYSROOT" ]; then
+  export WASI_SYSROOT="/opt/wasi-sysroot"
+fi
+
+export PATH="/usr/local/opt/llvm/bin:$PATH"
+
+export PREFIX="$(pwd)/libsodium-wasm32-wasi"
+
+mkdir -p $PREFIX || exit 1
+
+export CC="clang"
+export CFLAGS="--target=wasm32-unknkown-wasi --sysroot=${WASI_SYSROOT} -Os"
+export LDFLAGS="-s"
+export NM="llvm-nm"
+export AR="llvm-ar"
+export RANLIB="llvm-ranlib"
+export STRIP="llvm-strip"
+
+make distclean > /dev/null
+
+if [ -z "$LIBSODIUM_FULL_BUILD" ]; then
+  export LIBSODIUM_ENABLE_MINIMAL_FLAG="--enable-minimal"
+else
+  export LIBSODIUM_ENABLE_MINIMAL_FLAG=""
+fi
+
+./configure ${LIBSODIUM_ENABLE_MINIMAL_FLAG} \
+            --prefix="$PREFIX" --with-sysroot="$WASI_SYSROOT" \
+            --host=wasm32-unknown-wasi --disable-ssp --disable-shared || exit 1
+
+NPROCESSORS=$(getconf NPROCESSORS_ONLN 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
+PROCESSORS=${NPROCESSORS:-3}
+
+make -j${PROCESSORS} install || exit 1
+
+make distclean > /dev/null
