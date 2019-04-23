3fde7349e188424347bec70b1b6c74742004b5ea
Frank Denis <github@pureftpd.org>
2019-04-23 03:14:19 +0200

    Clarify that --min-reserved-size surprisingly sets the max memory

--- test/default/wasi-test-wrapper.sh
+++ test/default/wasi-test-wrapper.sh
@@ -1,24 +1,26 @@
 #! /bin/sh
 
+MAX_MEMORY_MB="128"
+
 if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "wasmtime" ]; then
   if command -v wasmtime >/dev/null; then
     wasmtime -o --dir=. "$1" && exit 0
   fi
 fi
 
-if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "wasmer" ]; then
-  if command -v wasmer >/dev/null; then
-    wasmer run "$1" --backend "${WASMER_BACKEND:-cranelift}" --dir=. && exit 0
-  fi
-fi
-
 if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "lucet" ]; then
   if command -v lucetc-wasi >/dev/null && command -v lucet-wasi >/dev/null; then
     lucetc-wasi \
-      --min-reserved-size "128MiB" \
+      --min-reserved-size "${MAX_MEMORY_MB}MiB" \
       -o "${1}.so" --opt-level best "$1" &&
       lucet-wasi --dir=.:. "${1}.so" &&
       rm -f "${1}.so" && exit 0
+  fi
+fi
+
+if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "wasmer" ]; then
+  if command -v wasmer >/dev/null; then
+    wasmer run "$1" --backend "${WASMER_BACKEND:-cranelift}" --dir=. && exit 0
   fi
 fi
 
