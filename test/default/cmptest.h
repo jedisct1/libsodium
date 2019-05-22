af6df5f4a591ea3216d80fb34694a3c292e9b51e
Frank Denis <github@pureftpd.org>
2019-05-23 00:38:14 +0200

    Revert "Limit resources when running tests"
    This reverts commit 9567bbe65fc64c870ea5e7925c809ba70c4a896b.

--- test/default/cmptest.h
+++ test/default/cmptest.h
@@ -14,18 +14,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(HAVE_SYS_RESOURCE_H) && defined(HAVE_SETRLIMIT)
-# include <sys/types.h>
-# include <sys/time.h>
-# include <sys/resource.h>
-#endif
-
 #include "sodium.h"
 #include "quirks.h"
-
-#ifndef MAX_MEMORY_TESTS
-# define MAX_MEMORY_TESTS 67108864
-#endif
 
 #ifdef __EMSCRIPTEN__
 # undef TEST_SRCDIR
@@ -46,25 +36,6 @@
 int xmain(void);
 
 static unsigned char *guard_page;
-
-static int set_resource_limits(void)
-{
-    int res = 0;
-
-#if defined(RLIM_INFINITY) && defined(HAVE_SETRLIMIT)
-    struct rlimit limits;
-
-    limits.rlim_cur = limits.rlim_max = MAX_MEMORY_TESTS;
-# ifdef RLIMIT_AS
-    res |= setrlimit(RLIMIT_AS, &limits);
-# endif
-# ifdef RLIMIT_DATA
-    res |= setrlimit(RLIMIT_DATA, &limits);
-# endif
-#endif
-
-    return res;
-}
 
 #ifdef BENCHMARKS
 
@@ -165,8 +136,6 @@ int main(void)
     unsigned long long ts_end;
     unsigned int       i;
 
-    (void) set_resource_limits();
-
     if (sodium_init() != 0) {
         return 99;
     }
@@ -201,8 +170,6 @@ int main(void)
     FILE          *fp_out;
     unsigned char *_guard_page;
     int           c;
-
-    (void) set_resource_limits();
 
     if ((fp_res = fopen(TEST_NAME_RES, "w+")) == NULL) {
         perror("fopen(" TEST_NAME_RES ")");
