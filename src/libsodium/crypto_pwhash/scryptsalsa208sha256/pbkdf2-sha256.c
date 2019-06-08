d3787c23b8d5f73dec31e17637d9a9f3639dce1d
Frank Denis <github@pureftpd.org>
2019-06-09 00:02:23 +0200

    Rename PBKDF2_SHA256 to escrypt_PBKDF2_SHA256

--- src/libsodium/crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.c
+++ src/libsodium/crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.c
@@ -39,13 +39,14 @@
 #include "utils.h"
 
 /**
- * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
+ * escrypt_PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
  * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
  * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
  */
 void
-PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt,
-              size_t saltlen, uint64_t c, uint8_t *buf, size_t dkLen)
+escrypt_PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen,
+                      const uint8_t *salt, size_t saltlen, uint64_t c,
+                      uint8_t *buf, size_t dkLen)
 {
     crypto_auth_hmacsha256_state PShctx, hctx;
     size_t                       i;
