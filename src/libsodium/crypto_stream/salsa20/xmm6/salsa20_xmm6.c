3747c68f72633a7dc8e7d3ee1d3df48221c327e8
Orivej Desh <orivej@gmx.fr>
2019-06-08 19:04:38 +0000

    Use crypto_stream_salsa20 private namespace for xmm6 implementation

--- src/libsodium/crypto_stream/salsa20/xmm6/salsa20_xmm6.c
+++ src/libsodium/crypto_stream/salsa20/xmm6/salsa20_xmm6.c
@@ -11,21 +11,21 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern int stream_salsa20_xmm6(unsigned char *c, unsigned long long clen,
-                               const unsigned char *n, const unsigned char *k);
+extern int crypto_stream_salsa20__xmm6(unsigned char *c, unsigned long long clen,
+                                       const unsigned char *n, const unsigned char *k);
 
-extern int stream_salsa20_xmm6_xor_ic(unsigned char *c, const unsigned char *m,
-                                      unsigned long long mlen,
-                                      const unsigned char *n,
-                                      uint64_t ic, const unsigned char *k);
+extern int crypto_stream_salsa20__xmm6_xor_ic(unsigned char *c, const unsigned char *m,
+                                              unsigned long long mlen,
+                                              const unsigned char *n,
+                                              uint64_t ic, const unsigned char *k);
 #ifdef __cplusplus
 }
 #endif
 
 struct crypto_stream_salsa20_implementation
     crypto_stream_salsa20_xmm6_implementation = {
-        SODIUM_C99(.stream =) stream_salsa20_xmm6,
-        SODIUM_C99(.stream_xor_ic =) stream_salsa20_xmm6_xor_ic,
+        SODIUM_C99(.stream =) crypto_stream_salsa20__xmm6,
+        SODIUM_C99(.stream_xor_ic =) crypto_stream_salsa20__xmm6_xor_ic,
     };
 
 #endif
