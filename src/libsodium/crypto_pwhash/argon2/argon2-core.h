a97ab7085fcda73f4fc7dead1a499025ceda9ecc
Frank Denis <github@pureftpd.org>
2019-06-10 20:07:54 +0200

    argon2_pick_best_implementation() can be static

--- src/libsodium/crypto_pwhash/argon2/argon2-core.h
+++ src/libsodium/crypto_pwhash/argon2/argon2-core.h
@@ -251,7 +251,6 @@ void argon2_finalize(const argon2_context *context,
  */
 typedef void (*fill_segment_fn)(const argon2_instance_t *instance,
                                 argon2_position_t        position);
-int argon2_pick_best_implementation(void);
 void argon2_fill_segment_avx512f(const argon2_instance_t *instance,
                                  argon2_position_t        position);
 void argon2_fill_segment_avx2(const argon2_instance_t *instance,
