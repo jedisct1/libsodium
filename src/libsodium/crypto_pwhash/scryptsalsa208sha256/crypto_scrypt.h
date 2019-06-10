95cf2e5da1b8009830966825fb8b23e8af84ce95
Frank Denis <github@pureftpd.org>
2019-06-10 20:07:16 +0200

    Style: remove unneeded extern

--- src/libsodium/crypto_pwhash/scryptsalsa208sha256/crypto_scrypt.h
+++ src/libsodium/crypto_pwhash/scryptsalsa208sha256/crypto_scrypt.h
@@ -61,38 +61,38 @@ typedef union {
 
 typedef escrypt_region_t escrypt_local_t;
 
-extern int escrypt_init_local(escrypt_local_t *__local);
+int escrypt_init_local(escrypt_local_t *__local);
 
-extern int escrypt_free_local(escrypt_local_t *__local);
+int escrypt_free_local(escrypt_local_t *__local);
 
-extern void *escrypt_alloc_region(escrypt_region_t *region, size_t size);
-extern int escrypt_free_region(escrypt_region_t *region);
+void *escrypt_alloc_region(escrypt_region_t *region, size_t size);
+int escrypt_free_region(escrypt_region_t *region);
 
 typedef int (*escrypt_kdf_t)(escrypt_local_t *__local, const uint8_t *__passwd,
                              size_t __passwdlen, const uint8_t *__salt,
                              size_t __saltlen, uint64_t __N, uint32_t __r,
                              uint32_t __p, uint8_t *__buf, size_t __buflen);
 
-extern int escrypt_kdf_nosse(escrypt_local_t *__local, const uint8_t *__passwd,
-                             size_t __passwdlen, const uint8_t *__salt,
-                             size_t __saltlen, uint64_t __N, uint32_t __r,
-                             uint32_t __p, uint8_t *__buf, size_t __buflen);
+int escrypt_kdf_nosse(escrypt_local_t *__local, const uint8_t *__passwd,
+                      size_t __passwdlen, const uint8_t *__salt,
+                      size_t __saltlen, uint64_t __N, uint32_t __r,
+                      uint32_t __p, uint8_t *__buf, size_t __buflen);
 
-extern int escrypt_kdf_sse(escrypt_local_t *__local, const uint8_t *__passwd,
-                           size_t __passwdlen, const uint8_t *__salt,
-                           size_t __saltlen, uint64_t __N, uint32_t __r,
-                           uint32_t __p, uint8_t *__buf, size_t __buflen);
+int escrypt_kdf_sse(escrypt_local_t *__local, const uint8_t *__passwd,
+                    size_t __passwdlen, const uint8_t *__salt,
+                    size_t __saltlen, uint64_t __N, uint32_t __r,
+                    uint32_t __p, uint8_t *__buf, size_t __buflen);
 
-extern uint8_t *escrypt_r(escrypt_local_t *__local, const uint8_t *__passwd,
-                          size_t __passwdlen, const uint8_t *__setting,
-                          uint8_t *__buf, size_t __buflen);
+uint8_t *escrypt_r(escrypt_local_t *__local, const uint8_t *__passwd,
+                   size_t __passwdlen, const uint8_t *__setting,
+                   uint8_t *__buf, size_t __buflen);
 
-extern uint8_t *escrypt_gensalt_r(uint32_t __N_log2, uint32_t __r, uint32_t __p,
-                                  const uint8_t *__src, size_t __srclen,
-                                  uint8_t *__buf, size_t __buflen);
+uint8_t *escrypt_gensalt_r(uint32_t __N_log2, uint32_t __r, uint32_t __p,
+                           const uint8_t *__src, size_t __srclen,
+                           uint8_t *__buf, size_t __buflen);
 
-extern const uint8_t *escrypt_parse_setting(const uint8_t *setting,
-                                            uint32_t *N_log2_p, uint32_t *r_p,
-                                            uint32_t *p_p);
+const uint8_t *escrypt_parse_setting(const uint8_t *setting,
+                                     uint32_t *N_log2_p, uint32_t *r_p,
+                                     uint32_t *p_p);
 
 #endif /* !_CRYPTO_SCRYPT_H_ */
