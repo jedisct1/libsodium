612d60f54af4edcc8a91c2a759d8504e596cf32e
Frank Denis <github@pureftpd.org>
2019-07-09 20:41:34 +0200

    lucetc-wasi requires units with --max-heap-size

--- test/default/wasi-test-wrapper.sh
+++ test/default/wasi-test-wrapper.sh
@@ -13,7 +13,7 @@ if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "lucet" ]; then
     lucetc-wasi \
       --reserved-size "${MAX_MEMORY_TESTS}" \
       -o "${1}.so" --opt-level fast "$1" &&
-      lucet-wasi --dir=.:. --max-heap-size "${MAX_MEMORY_TESTS}" "${1}.so" &&
+      lucet-wasi --dir=.:. --max-heap-size "${MAX_MEMORY_TESTS}B" "${1}.so" &&
       rm -f "${1}.so" && exit 0
   fi
 fi
