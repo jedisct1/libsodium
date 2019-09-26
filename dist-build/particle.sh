25251d72b3c054993f789462ecde1f8d454df168
Dan Kouba <dan.kouba@particle.io>
2019-09-26 16:33:15 -0700

    Modify build script: use variables, simplify/fix sed calls, remove makefiles from build output

--- dist-build/particle.sh
+++ dist-build/particle.sh
@@ -1,31 +1,29 @@
 #! /bin/sh
 
+BUILD_ROOT=builds/particle
+INCLUDE_DIR=src/libsodium/include
+
 # Build the Particle library structure
-[ -d "builds/particle" ] && rm -rf builds/particle
-mkdir builds/particle
-mkdir builds/particle/examples
-mkdir builds/particle/src
+[ -d  "$BUILD_ROOT" ] && rm -rf $BUILD_ROOT
+mkdir $BUILD_ROOT
+mkdir $BUILD_ROOT/examples
 
 # Copy over the libsodium code
-cp -r src/libsodium builds/particle/src/.
+cp -r src $BUILD_ROOT/.
 
 # Build version.h
-sed -i '' 's/@VERSION@/1.0.18/g' builds/particle/src/libsodium/include/sodium/version.h.in
-sed -i '' 's/@SODIUM_LIBRARY_VERSION_MAJOR@/10/g' builds/particle/src/libsodium/include/sodium/version.h.in
-sed -i '' 's/@SODIUM_LIBRARY_VERSION_MINOR@/3/g' builds/particle/src/libsodium/include/sodium/version.h.in
-sed -i '' 's/@SODIUM_LIBRARY_MINIMAL_DEF@//g' builds/particle/src/libsodium/include/sodium/version.h.in
-mv builds/particle/src/libsodium/include/sodium/version.h.in builds/particle/src/libsodium/include/sodium/version.h
+sed -i '' 's/@VERSION@/1.0.18/g; s/@SODIUM_LIBRARY_VERSION_MAJOR@/10/g; s/@SODIUM_LIBRARY_VERSION_MINOR@/3/g; s/@SODIUM_LIBRARY_MINIMAL_DEF@//g' $BUILD_ROOT/$INCLUDE_DIR/sodium/version.h.in
+mv $BUILD_ROOT/$INCLUDE_DIR/sodium/version.h.in $BUILD_ROOT/$INCLUDE_DIR/sodium/version.h
 
 # Modify structure to support Particle library format
-cp -r builds/particle/src/libsodium/include/*.h builds/particle/src/.
-cp -r builds/particle/src/libsodium/include/sodium/*.h builds/particle/src/.
-cp -r builds/particle/src/libsodium/include/sodium/private builds/particle/src/.
-# cp builds/particle/inc/sodium/private/*.h builds/particle/inc/.
-rm -r builds/particle/src/libsodium/include
-sed -i '' 's/sodium\///g' builds/particle/src/sodium.h
+cp -r $BUILD_ROOT/$INCLUDE_DIR/*.h $BUILD_ROOT/src/.
+cp -r $BUILD_ROOT/$INCLUDE_DIR/sodium/*.h $BUILD_ROOT/src/.
+cp -r $BUILD_ROOT/$INCLUDE_DIR/sodium/private $BUILD_ROOT/src/.
+rm -r $BUILD_ROOT/$INCLUDE_DIR
+sed -i '' 's/sodium\///g' $BUILD_ROOT/src/sodium.h
 
 # Add defines to tops of files so we don't have to use compiler directives
-find ./builds/particle/src -name \*.c -print -exec sed -i '' '1i\
+find $BUILD_ROOT/src -name \*.c -print -exec sed -i '' '1i\
 /*** Defines added by Particle build script ***/ \
 // Prevent warnings associated with building without running autogen.sh \
 #define CONFIGURED 1 \
@@ -40,15 +38,18 @@ find ./builds/particle/src -name \*.c -print -exec sed -i '' '1i\
 
 sed -i '' '1i\
 #define RANDOMBYTES_DEFAULT_IMPLEMENTATION
-' builds/particle/src/libsodium/randombytes/randombytes.c;
+' $BUILD_ROOT/src/libsodium/randombytes/randombytes.c;
 
 # Copy our code required for the Particle port
-cp -r particle/port/randombytes_particle.c builds/particle/src/.
-cp -r particle/port/randombytes_internal.h builds/particle/src/.
-cp particle/libsodium.cpp builds/particle/src/.
-cp particle/libsodium.h builds/particle/src/.
+cp -r particle/port/randombytes_particle.c $BUILD_ROOT/src/.
+cp -r particle/port/randombytes_internal.h $BUILD_ROOT/src/.
+cp particle/libsodium.cpp $BUILD_ROOT/src/.
+cp particle/libsodium.h $BUILD_ROOT/src/.
 
 # Copy over the the library metadata
-cp LICENSE builds/particle/.
-cp README.markdown builds/particle/README.md
-cp particle/library.properties builds/particle/.
+cp LICENSE $BUILD_ROOT/.
+cp README.markdown $BUILD_ROOT/README.md
+cp particle/library.properties $BUILD_ROOT/.
+
+# Clean up directory a little
+find $BUILD_ROOT/src -name Makefile\* -print -exec rm {} \;
