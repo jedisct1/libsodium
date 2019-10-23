bc973b966348df56912d99606b8e4fe673fdb909
Syrus <me@syrusakbary.com>
2019-10-22 20:06:09 -0700

    Simplify WASI build using wasienv

--- dist-build/wasm32-wasi.sh
+++ dist-build/wasm32-wasi.sh
@@ -1,36 +1,17 @@
 #! /bin/sh
 
-if [ -z "$WASI_LIBC" ]; then
-  for path in /opt/wasi-libc /opt/wasi-sysroot; do
-    if [ -d "$path" ]; then
-      export WASI_LIBC="$path"
-      break
-    fi
-  done
-fi
-if [ -z "$WASI_LIBC" ]; then
-  echo "Set WASI_LIBC to the path to the WASI libc sysroot" >&2
+if ! command -v wasienv >/dev/null; then
+  echo "Wasienv needs to be installed - https://github.com/wasienv/wasienv" >&2
   exit 1
 fi
-
-export PATH="/usr/local/opt/llvm/bin:$PATH"
 
 export PREFIX="$(pwd)/libsodium-wasm32-wasi"
 
 mkdir -p $PREFIX || exit 1
 
-export CC="clang"
-export CFLAGS="-DED25519_NONDETERMINISTIC=1 --target=wasm32-wasi --sysroot=${WASI_LIBC} -O2"
-export LDFLAGS="-s -Wl,--no-threads"
-export NM="llvm-nm"
-export AR="llvm-ar"
-export RANLIB="llvm-ranlib"
-export STRIP="llvm-strip"
+export CFLAGS="-DED25519_NONDETERMINISTIC=1 -O2"
 
 make distclean > /dev/null
-
-grep -q -F -- 'wasi' build-aux/config.sub || \
-  sed -i -e 's/-nacl\*)/-nacl*|-wasi)/' build-aux/config.sub
 
 if [ "x$1" = "x--bench" ]; then
   export BENCHMARKS=1
@@ -43,9 +24,8 @@ else
   export LIBSODIUM_ENABLE_MINIMAL_FLAG=""
 fi
 
-./configure ${LIBSODIUM_ENABLE_MINIMAL_FLAG} \
-            --prefix="$PREFIX" --with-sysroot="$WASI_LIBC" \
-            --host=wasm32-wasi \
+wasiconfigure ./configure ${LIBSODIUM_ENABLE_MINIMAL_FLAG} \
+            --prefix="$PREFIX" \
             --disable-ssp --disable-shared || exit 1
 
 NPROCESSORS=$(getconf NPROCESSORS_ONLN 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
--- test/default/wasi-test-wrapper.sh
+++ test/default/wasi-test-wrapper.sh
@@ -3,24 +3,24 @@
 MAX_MEMORY_TESTS="67108864"
 
 if command -v wasm-opt >/dev/null; then
-  wasm-opt -O4 -o "${1}.tmp" "$1" && mv -f "${1}.tmp" "$1"
+  wasm-opt -O4 -o "${1}.tmp" "$1.wasm" && mv -f "${1}.tmp" "$1.wasm"
 fi
 
 if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "wavm" ]; then
   if command -v wavm >/dev/null; then
-    wavm run --abi=wasi "$1" && exit 0
+    wavm run --abi=wasi "$1.wasm" && exit 0
   fi
 fi
 
 if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "wasmtime" ]; then
   if command -v wasmtime >/dev/null; then
-    wasmtime -o --dir=. "$1" && exit 0
+    wasmtime -o --dir=. "$1.wasm" && exit 0
   fi
 fi
 
 if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "wasmer" ]; then
   if command -v wasmer >/dev/null; then
-    wasmer run "$1" --backend "${WASMER_BACKEND:-cranelift}" --dir=. && exit 0
+    wasmer run "$1.wasm" --backend "${WASMER_BACKEND:-cranelift}" --dir=. && exit 0
   fi
 fi
 
@@ -28,7 +28,7 @@ if [ -z "$WASI_RUNTIME" ] || [ "$WASI_RUNTIME" = "lucet" ]; then
   if command -v lucetc-wasi >/dev/null && command -v lucet-wasi >/dev/null; then
     lucetc-wasi \
       --reserved-size "${MAX_MEMORY_TESTS}" \
-      -o "${1}.so" --opt-level 2 "$1" &&
+      -o "${1}.so" --opt-level 2 "$1.wasm" &&
       lucet-wasi --dir=.:. --max-heap-size "${MAX_MEMORY_TESTS}" "${1}.so" &&
       rm -f "${1}.so" && exit 0
   fi
