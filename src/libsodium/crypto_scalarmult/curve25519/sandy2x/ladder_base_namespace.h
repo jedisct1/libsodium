6a7fbccfd8c778247aead7acad4b68621485849c
Frank Denis <github@pureftpd.org>
2020-02-06 00:26:52 +0100

    Remove sandy2x fixed base scalar multiplication
    Thanks to precomputation, the generic implementation is faster.

--- src/libsodium/crypto_scalarmult/curve25519/sandy2x/ladder_base_namespace.h
+++
@@ -1,8 +0,0 @@
-#ifndef ladder_base_namespace_H
-#define ladder_base_namespace_H
-
-#define  ladder_base  _sodium_scalarmult_curve25519_sandy2x_ladder_base
-#define _ladder_base __sodium_scalarmult_curve25519_sandy2x_ladder_base
-
-#endif /* ifndef ladder_base_namespace_H */
-
