1088835f250d1a292aa1ca8932f3aaa9da22b2ee
Frank Denis <github@pureftpd.org>
2020-03-14 17:28:39 +0100

    Bring the MacOS minimum version to 10.10 (Yosemite)

--- dist-build/osx.sh
+++ dist-build/osx.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 export PREFIX="$(pwd)/libsodium-osx"
-export OSX_VERSION_MIN=${OSX_VERSION_MIN-"10.8"}
+export OSX_VERSION_MIN=${OSX_VERSION_MIN-"10.10"}
 export OSX_CPU_ARCH=${OSX_CPU_ARCH-"core2"}
 
 mkdir -p $PREFIX || exit 1
