85e48722a8f0b263943cc44d579f372a1e4cbc0d
Frank Denis <github@pureftpd.org>
2021-04-08 11:13:40 +0200

    Enable crypto extensions on ARMv8

--- dist-build/android-armv8-a.sh
+++ dist-build/android-armv8-a.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export TARGET_ARCH=armv8-a
+export TARGET_ARCH=armv8-a+crypto
 export CFLAGS="-Os -march=${TARGET_ARCH}"
 NDK_PLATFORM=android-21 ARCH=arm64 HOST_COMPILER=aarch64-linux-android "$(dirname "$0")/android-build.sh"
