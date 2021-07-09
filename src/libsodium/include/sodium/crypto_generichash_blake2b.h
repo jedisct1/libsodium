2805742ab31fd4610c76138c399e1952ff060544
infinitydev <github@byte-storm.com>
2021-07-09 10:56:51 +0000

    Fix incorrect XLC #pragma in public header

--- src/libsodium/include/sodium/crypto_generichash_blake2b.h
+++ src/libsodium/include/sodium/crypto_generichash_blake2b.h
@@ -24,7 +24,7 @@ typedef struct CRYPTO_ALIGN(64) crypto_generichash_blake2b_state {
     unsigned char opaque[384];
 } crypto_generichash_blake2b_state;
 
-#if defined(__IBMC__) || defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+#if defined(__SUNPRO_C) || defined(__SUNPRO_CC)
 # pragma pack()
 #else
 # pragma pack(pop)
