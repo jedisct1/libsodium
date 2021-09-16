64129657a5c67f3bab84562aa8d57dacc685cc75
Jørgen P. Tjernø <jorgen@tjer.no>
2021-09-16 11:01:24 -0700

    Portability improvements (#1105)
    * Move `raise` code to be behind an autoconf check
    
    This moves the call to `raise` behind a `HAVE_RAISE` autoconf check,
    in addition to `__wasm__`. This is intended to help porting to
    other platforms that don't support `raise` (e.g. modern game consoles).
    
    * Add autoconf check for `sysconf`
    
    Only try to invoke `sysconf` if the target platform supports it, and
    don't warn about unknown page size if `PAGE_SIZE` was defined. Add an
    include for `sys/param.h` to increase likelihood of finding `PAGE_SIZE`.
    This is intended to help porting to other platforms that don't support
    `sysconf` (e.g. modern game consoles) that have a fixed hardware page
    size.
    
    * Don't try to use raise & sysconf in a WASI environment
    
    Co-authored-by: Frank Denis <124872+jedisct1@users.noreply.github.com>

--- src/libsodium/sodium/utils.c
+++ src/libsodium/sodium/utils.c
@@ -9,12 +9,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef __wasm__
+#if defined(HAVE_RAISE) && !defined(__wasm__)
 # include <signal.h>
 #endif
 
 #ifdef HAVE_SYS_MMAN_H
 # include <sys/mman.h>
+#endif
+
+#ifdef HAVE_SYS_PARAM_H
+# include <sys/param.h>
 #endif
 
 #ifdef _WIN32
@@ -402,7 +406,7 @@ int
 _sodium_alloc_init(void)
 {
 #ifdef HAVE_ALIGNED_MALLOC
-# if defined(_SC_PAGESIZE)
+# if defined(_SC_PAGESIZE) && defined(HAVE_SYSCONF)
     long page_size_ = sysconf(_SC_PAGESIZE);
     if (page_size_ > 0L) {
         page_size = (size_t) page_size_;
@@ -411,7 +415,7 @@ _sodium_alloc_init(void)
     SYSTEM_INFO si;
     GetSystemInfo(&si);
     page_size = (size_t) si.dwPageSize;
-# else
+# elif !defined(PAGE_SIZE)
 #  warning Unknown page size
 # endif
     if (page_size < CANARY_SIZE || page_size < sizeof(size_t)) {
@@ -503,7 +507,7 @@ _mprotect_readwrite(void *ptr, size_t size)
 __attribute__((noreturn)) static void
 _out_of_bounds(void)
 {
-# ifndef __wasm__
+# if defined(HAVE_RAISE) && !defined(__wasm__)
 #  ifdef SIGSEGV
     raise(SIGSEGV);
 #  elif defined(SIGKILL)
