f3af42165a0f0ed58634257f4ed06e77f8c2a8c8
ReenigneCA <15387520+ReenigneCA@users.noreply.github.com>
2021-12-28 22:38:01 -0500

    Update android-armv7-a.sh
    changed the TARGET_ARCH variable to an ABI value that works with CMAKE_ANDROID_ARCH_ABI

--- dist-build/android-armv7-a.sh
+++ dist-build/android-armv7-a.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export TARGET_ARCH=armv7-a
-export CFLAGS="-Os -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb -marm -march=${TARGET_ARCH}"
+export TARGET_ARCH=armeabi-v7a
+export CFLAGS="-Os -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb -marm -march=armv7-a"
 ARCH=arm HOST_COMPILER=armv7a-linux-androideabi "$(dirname "$0")/android-build.sh"
+
