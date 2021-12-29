e016cfa752df56568fa15f89cca97196349c85ed
ReenigneCA <15387520+ReenigneCA@users.noreply.github.com>
2021-12-28 22:38:30 -0500

    Update android-armv8-a.sh
    changed the TARGET_ARCH variable to an ABI value that works with CMAKE_ANDROID_ARCH_ABI

--- dist-build/android-armv8-a.sh
+++ dist-build/android-armv8-a.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export TARGET_ARCH=armv8-a+crypto
-export CFLAGS="-Os -march=${TARGET_ARCH}"
+export TARGET_ARCH=arm64-v8a
+export CFLAGS="-Os -march=armv8-a"
 NDK_PLATFORM=android-21 ARCH=arm64 HOST_COMPILER=aarch64-linux-android "$(dirname "$0")/android-build.sh"
