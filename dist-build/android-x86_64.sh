4c5d602e86db879ca66110bdfdd207e934f4d74b
ReenigneCA <15387520+ReenigneCA@users.noreply.github.com>
2021-12-28 22:40:25 -0500

    Update android-x86_64.sh
    changed the TARGET_ARCH variable to an ABI value that works with CMAKE_ANDROID_ARCH_ABI

--- dist-build/android-x86_64.sh
+++ dist-build/android-x86_64.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export TARGET_ARCH=westmere
-export CFLAGS="-Os -march=${TARGET_ARCH}"
+export TARGET_ARCH=x86_64
+export CFLAGS="-Os -march=westmere"
 NDK_PLATFORM=android-21 ARCH=x86_64 HOST_COMPILER=x86_64-linux-android "$(dirname "$0")/android-build.sh"
