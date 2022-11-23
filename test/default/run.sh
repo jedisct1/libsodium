3075417ef81042f0bdf6b36140870789c00faf95
Frank Denis <github@pureftpd.org>
2022-11-23 21:32:10 +0100

    Add a simple test runner to zig-out/bin/

---
+++ test/default/run.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+find . -type f -perm +100 -print | grep -v run.sh | sort | while read -r x; do
+  echo "[$x]"
+  if ! "$x"; then
+    echo "*** [$x] FAILED" >&2
+    exit 1
+  fi
+done
