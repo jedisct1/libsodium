a990acec879f71367ba84e2bc172c41887c2e92d
murcoutinho <murilo9988@gmail.com>
2023-02-18 17:45:51 -0300

    creating forro folders

---
+++ src/libsodium/include/sodium/private/forro14_ietf_ext.h
@@ -0,0 +1,18 @@
+#ifndef chacha20_ietf_ext_H
+#define chacha20_ietf_ext_H
+
+#include <stdint.h>
+
+#include "private/quirks.h"
+
+/* The ietf_ext variant allows the internal counter to overflow into the IV */
+
+int crypto_stream_chacha20_ietf_ext(unsigned char *c, unsigned long long clen,
+                                    const unsigned char *n, const unsigned char *k);
+
+int crypto_stream_chacha20_ietf_ext_xor_ic(unsigned char *c, const unsigned char *m,
+                                           unsigned long long mlen,
+                                           const unsigned char *n, uint32_t ic,
+                                           const unsigned char *k);
+#endif
+
