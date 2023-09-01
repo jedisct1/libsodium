8ad54cb636316112de4a113fc51185ca56499bc8
Frank Denis <github@pureftpd.org>
2023-09-01 23:43:21 +0200

    Fallback to _Thread_local on C11 regardless of the platform

--- src/libsodium/randombytes/internal/randombytes_internal_random.c
+++ src/libsodium/randombytes/internal/randombytes_internal_random.c
@@ -96,13 +96,13 @@ BOOLEAN NTAPI RtlGenRandom(PVOID RandomBuffer, ULONG RandomBufferLength);
 # endif
 #endif
 
+#if !defined(TLS) && !defined(__STDC_NO_THREADS__) && \
+    defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+# define TLS _Thread_local
+#endif
 #ifndef TLS
 # ifdef _WIN32
-#  if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 201112L)
-#   define TLS _Thread_local
-#  else
-#   define TLS __declspec(thread)
-#  endif
+#  define TLS __declspec(thread)
 # else
 #  define TLS
 # endif
