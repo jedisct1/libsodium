d2694fc37d6a414fc7c7a2a88730dc79d91751d9
Saud Khan <dev@bidyut.com>
2024-09-11 22:04:43 +0800

    Add support for Android's new 16K page sizes (#1407)

--- dist-build/android-armv7-a.sh
+++ dist-build/android-armv7-a.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export TARGET_ARCH=armv7-a
 export CFLAGS="-Os -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb -marm -march=${TARGET_ARCH}"
+export LDFLAGS="-Wl,-z,max-page-size=16384"
 ARCH=arm HOST_COMPILER=armv7a-linux-androideabi "$(dirname "$0")/android-build.sh"
--- dist-build/android-x86_64.sh
+++ dist-build/android-x86_64.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export TARGET_ARCH=westmere
 export CFLAGS="-Os -march=${TARGET_ARCH}"
+export LDFLAGS="-Wl,-z,max-page-size=16384"
 NDK_PLATFORM=android-21 ARCH=x86_64 HOST_COMPILER=x86_64-linux-android "$(dirname "$0")/android-build.sh"
