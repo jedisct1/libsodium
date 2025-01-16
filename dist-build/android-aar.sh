151e71bf50794efc386b13789c99ce70b01c4e47
Frank Denis <github@pureftpd.org>
2025-01-16 14:36:11 +0100

    Set the default Android platform to 21
    Also explain that the "zip" command is required to build AAR archives
    and that NDK_PLATFORM can be used to override the target platform.
    
    Fixes #1440

--- dist-build/android-aar.sh
+++ dist-build/android-aar.sh
@@ -9,6 +9,13 @@ SODIUM_VERSION="1.0.20.0"
 NDK_VERSION=$(grep "Pkg.Revision = " <"${ANDROID_NDK_HOME}/source.properties" | cut -f 2 -d '=' | cut -f 2 -d' ' | cut -f 1 -d'.')
 DEST_PATH=$(mktemp -d)
 
+if which zip >/dev/null; then
+  echo "The 'zip' command is installed."
+else
+  echo "The 'zip' command is not installed. Please install it before running this script."
+  exit 1
+fi
+
 cd "$(dirname "$0")/../" || exit
 
 make_abi_json() {
