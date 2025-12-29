55f06e6738b2e597ba0735ddbd0c7b65505aea8a
Frank Denis <github@pureftpd.org>
2025-12-29 23:12:15 +0100

    Refer to the correct header

--- src/libsodium/crypto_core/ed25519/ref10/ed25519_ref10.c
+++ src/libsodium/crypto_core/ed25519/ref10/ed25519_ref10.c
@@ -39,7 +39,7 @@ load_4(const unsigned char *in)
  * and 10*25.5 bit limbs elsewhere.
  *
  * Functions used elsewhere that are candidates for inlining are defined
- * via "private/curve25519_ref10.h".
+ * via "private/ed25519_ref10.h".
  */
 
 #ifdef HAVE_TI_MODE
