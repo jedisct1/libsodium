f2da4cd8cb26599a0285a6ab0c02948e361a674a
Frank Denis <github@pureftpd.org>
2025-12-29 23:22:15 +0100

    core_ed25519_is_valid_point: check Y==Z in addition to X==0

--- src/libsodium/crypto_core/ed25519/ref10/ed25519_ref10.c
+++ src/libsodium/crypto_core/ed25519/ref10/ed25519_ref10.c
@@ -1044,10 +1044,13 @@ int
 ge25519_is_on_main_subgroup(const ge25519_p3 *p)
 {
     ge25519_p3 pl;
+    fe25519    t;
 
     ge25519_mul_l(&pl, p);
 
-    return fe25519_iszero(pl.X);
+    fe25519_sub(t, pl.Y, pl.Z);
+
+    return fe25519_iszero(pl.X) & fe25519_iszero(t);
 }
 
 int
