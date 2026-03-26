7d15af325d1bc6f20dd04df979c82c16e0e2ce2f
Frank Denis <github@pureftpd.org>
2026-03-26 23:52:20 +0800

    No need to include the hkdf headers twice

--- src/libsodium/include/sodium.h
+++ src/libsodium/include/sodium.h
@@ -30,8 +30,6 @@
 #include "sodium/crypto_hash_sha512.h"
 #include "sodium/crypto_ipcrypt.h"
 #include "sodium/crypto_kdf.h"
-#include "sodium/crypto_kdf_hkdf_sha256.h"
-#include "sodium/crypto_kdf_hkdf_sha512.h"
 #include "sodium/crypto_kdf_blake2b.h"
 #include "sodium/crypto_kem.h"
 #include "sodium/crypto_kem_mlkem768.h"
